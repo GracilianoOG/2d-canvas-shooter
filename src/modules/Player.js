6041e17db7527a2b1ccac37ae1349822dcef5788
Gabriel <gabriel.gracilianomb@gmail.com>
2023-10-20 15:53:03 -0300

    Player can move with wasd keys

--- src/modules/Player.js
+++ src/modules/Player.js
@@ -1,15 +1,40 @@
 class Player {
-  constructor(x, y, width, height, color) {
+  constructor(x, y, width, height, speed, color, keys) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.color = color;
+    this.speed = speed;
+    this.keys = keys;
   }
 
   draw(ctx) {
     ctx.fillStyle = this.color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+
+  move() {
+    if(this.keys["KeyA"]) {
+      this.x -= this.speed;
+    }
+
+    if(this.keys["KeyD"]) {
+      this.x += this.speed;
+    }
+
+    if(this.keys["KeyW"]) {
+      this.y -= this.speed;
+    }
+
+    if(this.keys["KeyS"]) {
+      this.y += this.speed;
+    }
+  }
+
+  update(ctx) {
+    this.draw(ctx);
+    this.move();
   }
 }
 
