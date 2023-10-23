ddf2d84d588a39f92d183b52b5b63b2baee01f70
Gabriel <gabriel.gracilianomb@gmail.com>
2023-10-23 13:29:14 -0300

    Added private fields to the Square class

--- src/modules/Square.js
+++ src/modules/Square.js
@@ -1,10 +1,29 @@
 import { Shape } from "./Shape.js";
 
 class Square extends Shape {
+  #width;
+  #height;
+
   constructor(x, y, width, height, color) {
     super(x, y, color);
-    this.width = width;
-    this.height = height;
+    this.#width = width;
+    this.#height = height;
+  }
+
+  set width(width) {
+    this.#width = width;
+  }
+
+  set height(height) {
+    this.#height = height;
+  }
+
+  get width() {
+    return this.#width;
+  }
+
+  get height() {
+    return this.#height;
   }
 
   draw(ctx) {
