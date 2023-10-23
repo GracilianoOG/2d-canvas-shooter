31fd4c0c64754856a776a5738804f7b237747cbd
Gabriel <gabriel.gracilianomb@gmail.com>
2023-10-23 13:28:54 -0300

    Added private fields to the Projectile class

--- src/modules/Projectile.js
+++ src/modules/Projectile.js
@@ -1,11 +1,38 @@
 import { Circle } from "./Circle.js";
 
 class Projectile extends Circle {
+  #speed;
+  #angle;
+  #hasCollided = false;
+
   constructor(x, y, radius, speed, angle, color) {
     super(x, y, radius, color);
-    this.speed = speed;
-    this.angle = angle;
-    this.hasCollided = false;
+    this.#speed = speed;
+    this.#angle = angle;
+  }
+
+  set speed(speed) {
+    this.#speed = speed;
+  }
+
+  set angle(angle) {
+    this.#angle = angle;
+  }
+
+  set hasCollided(hasCollided) {
+    this.#hasCollided = hasCollided;
+  }
+
+  get speed() {
+    return this.#speed;
+  }
+
+  get angle() {
+    return this.#angle;
+  }
+
+  get hasCollided() {
+    return this.#hasCollided;
   }
 
   move() {
