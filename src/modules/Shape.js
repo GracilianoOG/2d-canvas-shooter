aec6e9d973f447e6d23df6f0137ef4382f4da531
Gabriel <gabriel.gracilianomb@gmail.com>
2023-10-23 13:27:03 -0300

    Added private fields to the Shape class

--- src/modules/Shape.js
+++ src/modules/Shape.js
@@ -1,8 +1,36 @@
 class Shape {
+  #x;
+  #y;
+  #color;
+
   constructor(x, y, color) {
-    this.x = x;
-    this.y = y;
-    this.color = color;
+    this.#x = x;
+    this.#y = y;
+    this.#color = color;
+  }
+
+  set x(x) {
+    this.#x = x;
+  }
+
+  set y(y) {
+    this.#y = y;
+  }
+
+  set color(color) {
+    this.#color = color;
+  }
+
+  get x() {
+    return this.#x;
+  }
+
+  get y() {
+    return this.#y;
+  }
+
+  get color() {
+    return this.#color;
   }
 }
 
