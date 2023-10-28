507daa0c4c682cbdc1d92bb068ea6921f5d90559
Gabriel <gabriel.gracilianomb@gmail.com>
2023-10-28 15:26:49 -0300

    Implemented basic enemy spawning system

--- main.js
+++ main.js
@@ -10,18 +10,32 @@ const ctx = scene.getContext("2d");
 const bulletControl = new BulletControl(scene);
 const player = new Player(50, 50, 30, 30, 6, "white");
 const playerControl = new PlayerControl(player, scene, bulletControl);
-const enemy = new Enemy(50, 50, 20, 3, "red", player);
-const enemy2 = new Enemy(50, 50, 15, 4, "green", player);
-const enemy3 = new Enemy(50, 50, 10, 5, "blue", player);
+const enemies = [];
+
+const createHorizontalEnemy = (enemySize) => {
+  const xPos = Math.floor(Math.random() * scene.width);
+  const yPos = Math.random() > .5 ? -enemySize : scene.height + enemySize;
+  return [xPos, yPos];
+}
+
+const createVerticalEnemy = (enemySize) => {
+  const xPos = Math.random() > .5 ? -enemySize : scene.width + enemySize;
+  const yPos = Math.floor(Math.random() * scene.height);
+  return [xPos, yPos];
+}
+
+setInterval(() => {
+  const enemySize = 20;
+  const coords = Math.random() > .5 ? createVerticalEnemy(enemySize) : createHorizontalEnemy(enemySize);
+  enemies.push(new Enemy(coords[0], coords[1], enemySize, 3, "red", player));
+}, 2000);
 
 const animate = () => {
   ctx.clearRect(0, 0, scene.width, scene.height);
   requestAnimationFrame(animate);
   bulletControl.update();
   playerControl.update();
-  enemy.update(ctx);
-  enemy2.update(ctx);
-  enemy3.update(ctx);
+  enemies.forEach(enemy => enemy.update(ctx));
 }
 
 animate();
