8b4f86e07980c1ddda9ef03f395357174fc97e4c
Gabriel <gabriel.gracilianomb@gmail.com>
2023-10-27 22:35:20 -0300

    Added three enemies to follow player

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { PlayerControl } from "./src/modules/PlayerControl.js";
 import { Player } from "./src/modules/Player.js";
 import { BulletControl } from "./src/modules/BulletControl.js";
+import { Enemy } from "./src/modules/Enemy.js";
 
 const scene = document.querySelector("#scene");
 scene.width = innerWidth;
@@ -9,12 +10,18 @@ const ctx = scene.getContext("2d");
 const bulletControl = new BulletControl(scene);
 const player = new Player(50, 50, 30, 30, 6, "white");
 const playerControl = new PlayerControl(player, scene, bulletControl);
+const enemy = new Enemy(50, 50, 20, 3, "red", player);
+const enemy2 = new Enemy(50, 50, 15, 4, "green", player);
+const enemy3 = new Enemy(50, 50, 10, 5, "blue", player);
 
 const animate = () => {
   ctx.clearRect(0, 0, scene.width, scene.height);
   requestAnimationFrame(animate);
   bulletControl.update();
   playerControl.update();
+  enemy.update(ctx);
+  enemy2.update(ctx);
+  enemy3.update(ctx);
 }
 
 animate();
