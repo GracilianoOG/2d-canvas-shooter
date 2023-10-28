d75e5b9d7d48116d224016d7dfd61eb3bc5c03a8
Gabriel <gabriel.gracilianomb@gmail.com>
2023-10-28 16:01:42 -0300

    Removed enemy creation code and added EnemyControl

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { PlayerControl } from "./src/modules/PlayerControl.js";
 import { Player } from "./src/modules/Player.js";
 import { BulletControl } from "./src/modules/BulletControl.js";
-import { Enemy } from "./src/modules/Enemy.js";
+import { EnemyControl } from "./src/modules/EnemyControl.js";
 
 const scene = document.querySelector("#scene");
 scene.width = innerWidth;
@@ -10,32 +10,18 @@ const ctx = scene.getContext("2d");
 const bulletControl = new BulletControl(scene);
 const player = new Player(50, 50, 30, 30, 6, "white");
 const playerControl = new PlayerControl(player, scene, bulletControl);
-const enemies = [];
-
-const createHorizontalEnemy = (enemySize) => {
-  const xPos = Math.floor(Math.random() * scene.width);
-  const yPos = Math.random() > .5 ? -enemySize : scene.height + enemySize;
-  return [xPos, yPos];
-}
-
-const createVerticalEnemy = (enemySize) => {
-  const xPos = Math.random() > .5 ? -enemySize : scene.width + enemySize;
-  const yPos = Math.floor(Math.random() * scene.height);
-  return [xPos, yPos];
-}
-
-setInterval(() => {
-  const enemySize = 20;
-  const coords = Math.random() > .5 ? createVerticalEnemy(enemySize) : createHorizontalEnemy(enemySize);
-  enemies.push(new Enemy(coords[0], coords[1], enemySize, 3, "red", player));
-}, 2000);
+const enemyControl = new EnemyControl(scene, player);
 
 const animate = () => {
   ctx.clearRect(0, 0, scene.width, scene.height);
   requestAnimationFrame(animate);
   bulletControl.update();
   playerControl.update();
-  enemies.forEach(enemy => enemy.update(ctx));
+  enemyControl.update();
 }
+
+enemyControl.startEnemySpawn();
+
+setTimeout(() => enemyControl.stopEnemySpawn(), 6000);
 
 animate();
