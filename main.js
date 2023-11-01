5972b3ddeb61170a758fc0506376cb137181387a
Gabriel <gabriel.gracilianomb@gmail.com>
2023-10-31 22:32:00 -0300

    Removed canvas from html and added js canvas

--- main.js
+++ main.js
@@ -3,21 +3,30 @@ import { Player } from "./src/modules/Player.js";
 import { BulletControl } from "./src/modules/BulletControl.js";
 import { EnemyControl } from "./src/modules/EnemyControl.js";
 import { Collision } from "./src/modules/Collision.js";
+import { Canvas } from "./src/modules/Canvas.js";
 
-const canvas = document.querySelector("#canvas");
-canvas.width = innerWidth;
-canvas.height = innerHeight;
-const ctx = canvas.getContext("2d");
+// Canvas
+const canvasObj = new Canvas(
+  window.innerWidth, 
+  window.innerHeight, 
+  document.querySelector("#game-container")
+);
+const { canvas, context: ctx } = canvasObj;
+
+// Player
 const playerSize = 30;
 const player = new Player(
   canvas.width/2 - playerSize/2, 
   canvas.height/2 - playerSize/2, 
   playerSize, playerSize, 6, "white"
 );
+
+// Controllers
 const bulletControl = new BulletControl(canvas);
 const playerControl = new PlayerControl(player, canvas, bulletControl);
 const enemyControl = new EnemyControl(player, canvas);
 
+// Animation
 const animate = () => {
   requestAnimationFrame(animate);
   ctx.fillStyle = "rgba(0, 0, 0, .4)";
