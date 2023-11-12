95e55774af83e11ff3a678f7b67b2944ecb57d9a
Gabriel <gabriel.gracilianomb@gmail.com>
2023-11-12 19:38:13 -0300

    Added Scoreboard class

---
+++ src/modules/Scoreboard.js
@@ -0,0 +1,32 @@
+class Scoreboard {
+  #score = 0;
+  #length;
+  #scoreboard;
+
+  constructor(length, parent) {
+    this.#length = length;
+    this.#scoreboard = document.createElement("h2");
+    this.#scoreboard.classList.add("scoreboard");
+    this.#showScore(this.#score.toString());
+    parent.prepend(this.#scoreboard);
+  }
+
+  get score() {
+    return this.#score;
+  }
+
+  set score(score) {
+    this.score = score;
+    this.#showScore(score);
+  }
+
+  #formatScore(string) {
+    return string.padStart(this.#length, "0");
+  }
+
+  #showScore(score) {
+    this.#scoreboard.textContent = this.#formatScore(score.toString());
+  }
+}
+
+export { Scoreboard };
