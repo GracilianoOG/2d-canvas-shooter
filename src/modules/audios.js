8e67e9ceb2cb0acbdded8336c07cca03dfe5d585
Gabriel <gabriel.gracilianomb@gmail.com>
2023-11-20 19:52:32 -0300

    Added ogg format

--- src/modules/audios.js
+++ src/modules/audios.js
@@ -4,18 +4,18 @@ const musicPath = "./src/assets/audios/music/";
 const audios = {
   sounds: {
     hit: new Howl({
-      src: [`${soundPath}hitHurt.mp3`]
+      src: [`${soundPath}hitHurt.ogg`, `${soundPath}hitHurt.mp3`]
     }),
     explosion: new Howl({
-      src: [`${soundPath}explosion.mp3`]
+      src: [`${soundPath}explosion.ogg`, `${soundPath}explosion.mp3`]
     }),
     shot: new Howl({
-      src: [`${soundPath}laserShoot.mp3`]
+      src: [`${soundPath}laserShoot.ogg`, `${soundPath}laserShoot.mp3`]
     })
   },
   music: {
     battle: new Howl({
-      src: [`${musicPath}battle-loop.mp3`],
+      src: [`${musicPath}battle-loop.ogg`, `${musicPath}battle-loop.mp3`],
       loop: true,
       autoplay: true
     })
