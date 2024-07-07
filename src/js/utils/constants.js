9fa5be9fed0bdc31ab3b3d760c8ded8eba1decde
Gabriel <gabriel.gracilianomb@gmail.com>
2024-07-07 12:18:52 -0300

    Added file to store project constants

---
+++ src/js/utils/constants.js
@@ -0,0 +1,27 @@
+const CSS_CLASSES = {
+  GAME_START: ".game-start",
+  GAME_OVER: ".game-restart",
+  HIGHSCORE_POINTS: ".highscore-points",
+  SCORE: "score",
+  SCOREBOARD: "scoreboard"
+};
+
+const CSS_IDS = {
+  CONTAINER: "#game-container"
+};
+
+const STORAGE = {
+  KEY_POINTS: "js-shooter-highscore"
+};
+
+const COLORS = {
+  WHITE: "#fff",
+  TRANSPARENT_BLACK: "rgba(0, 0, 0, .4)"
+};
+
+export {
+  CSS_CLASSES,
+  CSS_IDS,
+  COLORS,
+  STORAGE
+};
