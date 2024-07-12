92dcca3b453a97e43eccba45646a8ad9c88bb767
Gabriel <gabriel.gracilianomb@gmail.com>
2024-07-12 14:35:00 -0300

    Add function to store score

--- src/js/utils/helpers.js
+++ src/js/utils/helpers.js
@@ -13,7 +13,16 @@ const getHighscore = (isFormatted = true) => {
   return highscore
 }
 
+const storeHighscore = (score) => {
+  const KEY = STORAGE.KEY_POINTS;
+  const highscore = parseInt(localStorage.getItem(KEY) || 0);
+  if(score > highscore) {
+    localStorage.setItem(KEY, score);
+  }
+}
+
 export {
   formatScore,
-  getHighscore
+  getHighscore,
+  storeHighscore
 }
