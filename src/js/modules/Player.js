3f32000bd496c271824feaff15531dcd035ccedf
Gabriel <gabriel.gracilianomb@gmail.com>
2024-07-13 14:34:45 -0300

    Remove inheritance from Player
    Circle is now a value from the "shape" property. Additional temporary methods were created to avoid breaking the whole game.

--- src/js/modules/Player.js
+++ src/js/modules/Player.js
@@ -1,13 +1,52 @@
 import { Circle } from "./Circle.js";
 
-class Player extends Circle {
+class Player {
   #speed;
   #isDead = false;
+  #shape;
 
   constructor(x, y, radius, speed, color) {
-    super(x, y, radius, color);
+    this.#shape = new Circle(x, y, radius, color);
     this.#speed = speed;
   }
+
+  // MESS
+  get x() {
+    return this.#shape.x;
+  }
+
+  set x(x) {
+    this.#shape.x = x;
+  }
+
+  get y() {
+    return this.#shape.y;
+  }
+
+  set y(y) {
+    this.#shape.y = y;
+  }
+
+  get color() {
+    return this.#shape.color;
+  }
+
+  set color(color) {
+    this.#shape.color = color;
+  }
+
+  collidedWith(object) {
+    return this.#shape.collidedWith(object);
+  }
+
+  get radius() {
+    return this.#shape.dimensions.radius;
+  }
+
+  set radius(radius) {
+    this.#shape.dimensions = { radius };
+  }
+  // MESS
 
   get speed() {
     return this.#speed;
@@ -26,7 +65,7 @@ class Player extends Circle {
   }
 
   update(ctx) {
-    this.draw(ctx);
+    this.#shape.draw(ctx);
   }
 }
 
