db97211d695a7b9c35e6d16dd34417bdd40a7102
Gabriel <gabriel.gracilianomb@gmail.com>
2024-07-13 12:58:01 -0300

    Create enemy list once

--- src/js/modules/EnemyCreator.js
+++ src/js/modules/EnemyCreator.js
@@ -6,11 +6,20 @@ class EnemyCreator {
   #enemies;
   #intervalId;
   #currentSpawnRate;
+  #enemyTypes;
 
   constructor({ player, mainCanvas, enemies }) {
     this.#target = player;
     this.#canvas = mainCanvas.canvas;
     this.#enemies = enemies;
+    this.#enemyTypes = [
+      { hp: 20, radius: 18, speed: 4, color: "#ff0000" },
+      { hp: 10, radius: 14, speed: 5, color: "#f210c8" },
+      { hp: 30, radius: 25, speed: 3, color: "#10b2f2" },
+      { hp: 30, radius: 20, speed: 4, color: "#021ffc" },
+      { hp: 50, radius: 30, speed: 2, color: "#1ff40c" },
+      { hp: 10, radius: 10, speed: 6, color: "#fc4d02" }
+    ];
 
     document.addEventListener("visibilitychange", () => {
       if(this.#target.isDead) {
@@ -40,18 +49,8 @@ class EnemyCreator {
   }
 
   #randomizeEnemy() {
-    const enemyTypes = [
-      { hp: 20, radius: 18, speed: 4, color: "#ff0000" },
-      { hp: 10, radius: 14, speed: 5, color: "#f210c8" },
-      { hp: 30, radius: 25, speed: 3, color: "#10b2f2" },
-      { hp: 30, radius: 20, speed: 4, color: "#021ffc" },
-      { hp: 50, radius: 30, speed: 2, color: "#1ff40c" },
-      { hp: 10, radius: 10, speed: 6, color: "#fc4d02" }
-    ];
-    const randomSeed = Math.floor(Math.random() * enemyTypes.length);
-    console.log(randomSeed)
-
-    return enemyTypes[randomSeed];
+    const randomSeed = Math.floor(Math.random() * this.#enemyTypes.length);
+    return this.#enemyTypes[randomSeed];
   }
 
   #createEnemy() {
