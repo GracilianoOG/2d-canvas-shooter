498216d72da13259bbebcd80f727ac1a64816c49
Gabriel <gabriel.gracilianomb@gmail.com>
2024-07-13 14:44:01 -0300

    Create generic Entity

---
+++ src/js/modules/Entity.js
@@ -0,0 +1,37 @@
+class Entity {
+  #shape;
+
+  get x() {
+    return this.#shape.x;
+  }
+
+  set x(x) {
+    this.#shape.x = x;
+  }
+
+  get y() {
+    return this.#shape.y;
+  }
+
+  set y(y) {
+    this.#shape.y = y;
+  }
+
+  get color() {
+    return this.#shape.color;
+  }
+
+  set color(color) {
+    this.#shape.color = color;
+  }
+
+  get dimensions() {
+    return this.#shape.dimensions;
+  }
+
+  set dimensions(dimensions) {
+    this.#shape.dimensions = dimensions;
+  }
+}
+
+export { Entity }
