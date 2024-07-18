cf8e3a730ed8c5fb77001ebdf59211b03e9d4282
Gabriel <gabriel.gracilianomb@gmail.com>
2024-07-18 11:48:39 -0300

    Remove update method from GameState
    This will break the game.

--- src/js/modules/GameState.js
+++ src/js/modules/GameState.js
@@ -13,14 +13,6 @@ class GameState {
   get entities() {
     return this.#entities;
   }
-
-  updateObjects(array) {
-    for(let i = array.length - 1; i >= 0; i--) {
-      const elem = array[i];
-      elem.update(this.#entities.mainCanvas.context);
-      elem.toDestroy && array.splice(i, 1);
-    }
-  }
 }
 
 export { GameState };
