25aeb734c2d1008510f92e3b1e00273662502918
Gabriel <gabriel.gracilianomb@gmail.com>
2024-09-30 20:31:44 -0300

    Fix path of audios

--- src/js/modules/audios.js
+++ src/js/modules/audios.js
@@ -1,16 +1,16 @@
 import { Howl } from "howler";
 
-import hitOgg from "/audios/sounds/hitHurt.ogg";
-import hitMp3 from "/audios/sounds/hitHurt.mp3";
+import hitOgg from "/assets/audios/sounds/hitHurt.ogg";
+import hitMp3 from "/assets/audios/sounds/hitHurt.mp3";
 
-import explosionOgg from "/audios/sounds/explosion.ogg";
-import explosionMp3 from "/audios/sounds/explosion.mp3";
+import explosionOgg from "/assets/audios/sounds/explosion.ogg";
+import explosionMp3 from "/assets/audios/sounds/explosion.mp3";
 
-import laserOgg from "/audios/sounds/laserShoot.ogg";
-import laserMp3 from "/audios/sounds/laserShoot.mp3";
+import laserOgg from "/assets/audios/sounds/laserShoot.ogg";
+import laserMp3 from "/assets/audios/sounds/laserShoot.mp3";
 
-import battleOgg from "/audios/music/battle-loop.ogg";
-import battleMp3 from "/audios/music/battle-loop.mp3";
+import battleOgg from "/assets/audios/music/battle-loop.ogg";
+import battleMp3 from "/assets/audios/music/battle-loop.mp3";
 
 const audios = {
   sounds: {
