8eaf115c4045d0268b218d0b385844d24c5ce7b2
Gabriel <gabriel.gracilianomb@gmail.com>
2025-02-21 15:44:20 -0300

    Remove entities property

--- src/js/modules/EnemyCreator.js
+++ src/js/modules/EnemyCreator.js
@@ -1,28 +1,26 @@
 import { Enemy } from "./Enemy.js";
 
 class EnemyCreator {
-  #entities;
   #intervalId;
   #currentSpawnRate;
   #enemyTypes;
 
-  constructor(entities) {
-    this.#entities = entities;
+  constructor() {
     this.#enemyTypes = [
       { hp: 20, radius: 18, speed: 4, color: "#ff0000" },
       { hp: 10, radius: 14, speed: 5, color: "#f210c8" },
       { hp: 30, radius: 25, speed: 3, color: "#10b2f2" },
       { hp: 30, radius: 20, speed: 4, color: "#021ffc" },
       { hp: 50, radius: 30, speed: 2, color: "#1ff40c" },
-      { hp: 10, radius: 10, speed: 6, color: "#fc4d02" }
+      { hp: 10, radius: 10, speed: 6, color: "#fc4d02" },
     ];
 
     document.addEventListener("visibilitychange", () => {
-      if(this.#entities.player.isDead) {
+      if (window.gameState["entities"].player.isDead) {
         return;
       }
 
-      if(document.hidden) {
+      if (document.hidden) {
         this.stopEnemySpawn();
       } else {
         this.restartEnemySpawn();
@@ -33,14 +31,24 @@ class EnemyCreator {
   #createEnemyPosition(enemySize) {
     let xPos, yPos;
 
-    if(Math.random() > .5) {
-      xPos = Math.random() > .5 ? -enemySize : this.#entities.mainCanvas.width + enemySize;
-      yPos = Math.floor(Math.random() * this.#entities.mainCanvas.height);
+    if (Math.random() > 0.5) {
+      xPos =
+        Math.random() > 0.5
+          ? -enemySize
+          : window.gameState["entities"].mainCanvas.width + enemySize;
+      yPos = Math.floor(
+        Math.random() * window.gameState["entities"].mainCanvas.height
+      );
       return [xPos, yPos];
     }
 
-    xPos = Math.floor(Math.random() * this.#entities.mainCanvas.width);
-    yPos = Math.random() > .5 ? -enemySize : this.#entities.mainCanvas.height + enemySize;
+    xPos = Math.floor(
+      Math.random() * window.gameState["entities"].mainCanvas.width
+    );
+    yPos =
+      Math.random() > 0.5
+        ? -enemySize
+        : window.gameState["entities"].mainCanvas.height + enemySize;
     return [xPos, yPos];
   }
 
@@ -52,18 +60,26 @@ class EnemyCreator {
   #createEnemy() {
     const { hp, radius, speed, color } = this.#randomizeEnemy();
     const coords = this.#createEnemyPosition(radius);
-    this.#entities.enemies.push(
-      new Enemy(coords[0], coords[1], radius, speed, color, hp, this.#entities.player)
+    window.gameState["entities"].enemies.push(
+      new Enemy(
+        coords[0],
+        coords[1],
+        radius,
+        speed,
+        color,
+        hp,
+        window.gameState["entities"].player
+      )
     );
   }
 
   startEnemySpawn(secondsToSpawn) {
-    if(this.#intervalId) {
+    if (this.#intervalId) {
       throw "Multiple intervals cannot be started. Clear the current interval before starting a new one.";
     }
     this.#currentSpawnRate = secondsToSpawn;
     this.#intervalId = setInterval(() => {
-      if(this.#entities.player.isDead) {
+      if (window.gameState["entities"].player.isDead) {
         this.stopEnemySpawn();
       }
 
