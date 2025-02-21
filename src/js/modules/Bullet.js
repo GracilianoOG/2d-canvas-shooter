945dac2dd2b2de56da1907bf84ba38404baa0c9d
Gabriel <gabriel.gracilianomb@gmail.com>
2025-02-21 15:58:57 -0300

    Add damage to bullets

--- src/js/modules/Bullet.js
+++ src/js/modules/Bullet.js
@@ -2,10 +2,12 @@ import { Projectile } from "./Projectile.js";
 
 class Bullet extends Projectile {
   #angle;
+  #damage;
 
-  constructor(x, y, radius, speed, angle, color) {
+  constructor(x, y, radius, speed, angle, color, damage = 10) {
     super(x, y, radius, speed, color);
     this.#angle = angle;
+    this.#damage = damage;
   }
 
   get angle() {
@@ -16,8 +18,16 @@ class Bullet extends Projectile {
     this.#angle = angle;
   }
 
+  get damage() {
+    return this.#damage;
+  }
+
   #isOutOfCanvas(canvas) {
-    const { x: bx, y: by, dimensions: { radius: br } } = this;
+    const {
+      x: bx,
+      y: by,
+      dimensions: { radius: br },
+    } = this;
     const { width: cw, height: ch } = canvas;
     return bx < -br || bx > cw + br || by < -br || by > ch + br;
   }
@@ -30,7 +40,7 @@ class Bullet extends Projectile {
   update(ctx) {
     this.draw(ctx);
     this.#move();
-    if(!this.toDestroy && this.#isOutOfCanvas(ctx.canvas)) {
+    if (!this.toDestroy && this.#isOutOfCanvas(ctx.canvas)) {
       this.toDestroy = true;
     }
   }
