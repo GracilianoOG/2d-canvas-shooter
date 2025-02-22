5c1db6717c6bb6cb9aa8185fcf9f99032a03c119
Gabriel <gabriel.gracilianomb@gmail.com>
2025-02-22 20:01:00 -0300

    Add initial menu presentation to main

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 import { Game } from "./modules/Game.js";
+import { Scoreboard } from "./modules/Scoreboard.js";
+import { CSS_CLASSES } from "./utils/constants.js";
 
-new Game();
+const highscoreBoard = document.querySelector(CSS_CLASSES.HIGHSCORE_POINTS);
+const startScreen = document.querySelector(CSS_CLASSES.GAME_START);
+highscoreBoard.textContent = Scoreboard.retrieveHighscore();
+
+const game = new Game();
+
+startScreen.addEventListener(
+  "click",
+  e => {
+    e.stopPropagation();
+    startScreen.classList.add("hide");
+    game.init();
+  },
+  { once: true }
+);
