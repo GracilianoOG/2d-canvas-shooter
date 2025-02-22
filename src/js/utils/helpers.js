4b16724c6c4a59cf12bfb7898e7b5a06f731a874
Gabriel <gabriel.gracilianomb@gmail.com>
2025-02-22 10:33:59 -0300

    Remove localStorage functions

--- src/js/utils/helpers.js
+++ src/js/utils/helpers.js
@@ -1,40 +1,14 @@
-import { CSS_CLASSES, STORAGE } from "./constants.js";
+import { CSS_CLASSES } from "./constants.js";
 
-const formatScore = (string, length = 8) => {
-  return string.padStart(length, "0");
-}
-
-const getHighscore = (isFormatted = true) => {
-  const highscore = localStorage.getItem(STORAGE.KEY_POINTS) || ""
-
-  if(isFormatted) {
-    return formatScore(highscore)
-  }
-  return highscore
-}
-
-const storeHighscore = (score) => {
-  const KEY = STORAGE.KEY_POINTS;
-  const highscore = parseInt(localStorage.getItem(KEY) || 0);
-  if(score > highscore) {
-    localStorage.setItem(KEY, score);
-  }
-}
-
-const notifyScoreEarned = (x, y, scoreAmount) => {
+export const notifyScoreEarned = (x, y, scoreAmount) => {
   const score = document.createElement("div");
   score.setAttribute("class", CSS_CLASSES.SCORE);
   score.textContent = scoreAmount;
   score.style.left = x + "px";
   score.style.top = y + "px";
 
-  score.addEventListener("animationend", () => document.body.removeChild(score));
+  score.addEventListener("animationend", () =>
+    document.body.removeChild(score)
+  );
   document.body.appendChild(score);
-}
-
-export {
-  formatScore,
-  getHighscore,
-  storeHighscore,
-  notifyScoreEarned
-}
+};
