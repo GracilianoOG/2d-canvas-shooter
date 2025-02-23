5b943cd45ca1e106c20cbc7f7dc754005ccb4d47
Gabriel <gabriel.gracilianomb@gmail.com>
2025-02-23 15:41:05 -0300

    Add color parameter to StatusIndicator

--- src/js/modules/StatusIndicator.js
+++ src/js/modules/StatusIndicator.js
@@ -1,12 +1,13 @@
 import { CSS_CLASSES } from "../utils/constants";
 
 class StatusIndicator {
-  static create(x, y, score, container) {
+  static create(x, y, score, container, color = "#fff") {
     const scoreEl = document.createElement("div");
     scoreEl.setAttribute("class", CSS_CLASSES.SCORE);
     scoreEl.textContent = score;
     scoreEl.style.left = `${x}px`;
     scoreEl.style.top = `${y}px`;
+    scoreEl.style.color = color;
 
     scoreEl.addEventListener(
       "animationend",
