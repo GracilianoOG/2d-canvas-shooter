f41a4072cbeb96890be39cb15d38b80c1f8f26f4
Gabriel <gabriel.gracilianomb@gmail.com>
2025-02-23 22:58:46 -0300

    Remove toDestroy code from Projectile

--- src/js/modules/Projectile.js
+++ src/js/modules/Projectile.js
@@ -2,19 +2,9 @@ import { Circle } from "./Circle.js";
 import { Entity } from "./Entity.js";
 
 class Projectile extends Entity {
-  #toDestroy = false;
-
   constructor(x, y, radius, speed, color) {
     super(new Circle(x, y, radius, color));
     this.speed = speed;
-  }
-
-  get toDestroy() {
-    return this.#toDestroy;
-  }
-
-  set toDestroy(toDestroy) {
-    this.#toDestroy = toDestroy;
   }
 }
 
