1ed0c64bca875806e4125d9e7d7d1a283878775a
Gabriel <gabriel.gracilianomb@gmail.com>
2025-02-26 15:53:48 -0300

    Remove unneeded code of InputHandler

--- src/scripts/InputHandler.js
+++ src/scripts/InputHandler.js
@@ -1,32 +1,9 @@
 class InputHandler {
-  static handlers = {};
-
-  static create(
-    type,
-    action,
-    pausable = true,
-    target = document,
-    group = "game"
-  ) {
+  static create(type, action, pausable = true, target = document) {
     const handler = pausable
-      ? (...args) => window.gameState["entities"].isRunning && action(...args)
+      ? (...args) => window.gameState.entities.isRunning && action(...args)
       : action;
-    InputHandler.handlers[group] = InputHandler.handlers[group] ?? {};
-    if (InputHandler.handlers[group][type]) {
-      console.error(`"${type}" is already registered in group "${group}"`);
-      return;
-    }
-    InputHandler.handlers[group][type] = {};
-    InputHandler.handlers[group][type].action = handler;
-    InputHandler.handlers[group][type].target = target;
     target.addEventListener(type, handler);
-    console.log(InputHandler.handlers);
-  }
-
-  static remove(type, group = "game") {
-    const target = InputHandler.handlers[group][type].target;
-    target.removeEventListener(type, InputHandler.handlers[group][type]);
-    // TODO: also remove from handlers
   }
 }
 
