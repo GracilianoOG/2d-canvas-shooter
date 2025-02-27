8ae33aaf33db9ecd7d95d8bc138f3b8de1df2f4c
Gabriel <gabriel.gracilianomb@gmail.com>
2025-02-27 17:32:34 -0300

    Refactor PlayerController

--- src/scripts/player/PlayerController.js
+++ src/scripts/player/PlayerController.js
@@ -27,48 +27,39 @@ class PlayerController {
 
   #moveLeft() {
     this.#player.x -= this.#player.speed;
-    if (this.#player.x < this.#player.dimensions.radius) {
+    if (this.#player.isTouchingBorders()) {
       this.#player.x = this.#player.dimensions.radius;
     }
   }
 
-  #moveRight(canvasWidth) {
+  #moveRight() {
+    const canvas = window.gameState.entities.mainCanvas.canvas;
     this.#player.x += this.#player.speed;
-    if (this.#player.x + this.#player.dimensions.radius > canvasWidth) {
-      this.#player.x = canvasWidth - this.#player.dimensions.radius;
+    if (this.#player.isTouchingBorders()) {
+      this.#player.x = canvas.width - this.#player.dimensions.radius;
     }
   }
 
   #moveUp() {
     this.#player.y -= this.#player.speed;
-    if (this.#player.y < this.#player.dimensions.radius) {
+    if (this.#player.isTouchingBorders()) {
       this.#player.y = this.#player.dimensions.radius;
     }
   }
 
-  #moveDown(canvasHeight) {
+  #moveDown() {
+    const canvas = window.gameState.entities.mainCanvas.canvas;
     this.#player.y += this.#player.speed;
-    if (this.#player.y + this.#player.dimensions.radius > canvasHeight) {
-      this.#player.y = canvasHeight - this.#player.dimensions.radius;
+    if (this.#player.isTouchingBorders()) {
+      this.#player.y = canvas.height - this.#player.dimensions.radius;
     }
   }
 
   #movePlayer() {
-    if (this.#keys["KeyA"]) {
-      this.#moveLeft();
-    }
-
-    if (this.#keys["KeyD"]) {
-      this.#moveRight(window.gameState["entities"].mainCanvas.width);
-    }
-
-    if (this.#keys["KeyW"]) {
-      this.#moveUp();
-    }
-
-    if (this.#keys["KeyS"]) {
-      this.#moveDown(window.gameState["entities"].mainCanvas.height);
-    }
+    if (this.#keys["KeyA"]) this.#moveLeft();
+    if (this.#keys["KeyD"]) this.#moveRight();
+    if (this.#keys["KeyW"]) this.#moveUp();
+    if (this.#keys["KeyS"]) this.#moveDown();
   }
 
   update() {
