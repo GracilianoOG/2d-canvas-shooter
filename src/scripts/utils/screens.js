3d03fe67aa7377d7146deb72a466274e7ab25872
Gabriel <gabriel.gracilianomb@gmail.com>
2025-02-27 11:07:56 -0300

    Add screens with dom refs

---
+++ src/scripts/utils/screens.js
@@ -0,0 +1,4 @@
+export const game = document.querySelector("#game-container");
+export const start = document.querySelector(".start-screen");
+export const pause = document.querySelector(".pause-screen");
+export const restart = document.querySelector(".gameover-screen");
