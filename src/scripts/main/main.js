0cc1d3e5dc6ef3d477485c9120e8beec6a14e74c
Gabriel <gabriel.gracilianomb@gmail.com>
2025-02-27 11:08:25 -0300

    Get screens from utils

--- src/scripts/main/main.js
+++ src/scripts/main/main.js
@@ -1,22 +1,21 @@
 import { Game } from "../game/Game.js";
 import { Scoreboard } from "../score/Scoreboard.js";
 import { CSS_CLASSES } from "../utils/constants.js";
+import { pause, start } from "../utils/screens.js";
 
 const highscoreBoard = document.querySelector(CSS_CLASSES.HIGHSCORE_POINTS);
-const startScreen = document.querySelector(CSS_CLASSES.GAME_START);
-const pauseScreen = document.querySelector(".pause-screen");
 highscoreBoard.textContent = Scoreboard.retrieveHighscore();
 
 const game = new Game();
 
-startScreen.addEventListener(
+start.addEventListener(
   "click",
   e => {
     e.stopPropagation();
-    startScreen.classList.add("hide");
+    start.classList.add("hide");
     game.init();
   },
   { once: true }
 );
 
-pauseScreen.addEventListener("click", () => game.pause());
+pause.addEventListener("click", () => game.pause());
