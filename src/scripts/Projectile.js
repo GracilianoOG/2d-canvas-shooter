3b63f50be75110556eb4d8d6a5624c34f0979cf5
Gabriel <gabriel.gracilianomb@gmail.com>
2025-02-28 10:51:25 -0300

    Add speed manipulation in Projectile

--- src/scripts/Projectile.js
+++ src/scripts/Projectile.js
@@ -2,9 +2,19 @@ import { Circle } from "./Circle.js";
 import { Entity } from "./Entity.js";
 
 class Projectile extends Entity {
+  #speed;
+
   constructor(x, y, radius, speed, color) {
     super(new Circle(x, y, radius, color));
     this.speed = speed;
+  }
+
+  get speed() {
+    return this.#speed;
+  }
+
+  set speed(speed) {
+    this.#speed = speed;
   }
 }
 
