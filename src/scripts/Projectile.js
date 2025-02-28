6afccb453e7810ed94c5e1df7f905ab4146a0289
Gabriel <gabriel.gracilianomb@gmail.com>
2025-02-28 11:13:31 -0300

    Pass parameters of new super in Projectile

--- src/scripts/Projectile.js
+++ src/scripts/Projectile.js
@@ -1,11 +1,10 @@
-import { Circle } from "./Circle.js";
 import { Entity } from "./Entity.js";
 
 class Projectile extends Entity {
   #speed;
 
   constructor(x, y, radius, speed, color) {
-    super(new Circle(x, y, radius, color));
+    super(x, y, radius, color);
     this.speed = speed;
   }
 
