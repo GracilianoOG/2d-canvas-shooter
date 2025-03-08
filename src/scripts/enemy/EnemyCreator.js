cc46feaf5efcedc4446d2ed49890501f1f829c5b
Gabriel <gabriel.gracilianomb@gmail.com>
2025-03-08 17:33:22 -0300

    Add method to modify enemy
    This will make the game a little harder and will give extra points to the player.

--- src/scripts/enemy/EnemyCreator.js
+++ src/scripts/enemy/EnemyCreator.js
@@ -1,16 +1,13 @@
 import { gameState } from "../singletons/GameState.js";
 import { Timer } from "../Timer.js";
-import { minOrMaxPoint, randomLinePoint } from "../utils/utility.js";
+import { minOrMaxPoint, randomInt, randomLinePoint } from "../utils/utility.js";
 import { Enemy } from "./Enemy.js";
 import { enemyTypes } from "./enemyTypes.js";
 
 class EnemyCreator {
   constructor(spawnTime) {
-    this.timer = new Timer(
-      spawnTime,
-      { autostart: false },
-      this.#createEnemy.bind(this)
-    );
+    const noStart = { autostart: false };
+    this.timer = new Timer(spawnTime, noStart, this.#createEnemy.bind(this));
   }
 
   #createEnemyPosition(enemySize) {
@@ -24,8 +21,34 @@ class EnemyCreator {
   }
 
   #randomizeEnemy() {
-    const randomSeed = Math.floor(Math.random() * enemyTypes.length);
-    return enemyTypes[randomSeed];
+    const rndSeed = Math.floor(Math.random() * enemyTypes.length);
+    const rndEnemy = { ...enemyTypes[rndSeed] };
+    if (randomInt(0, 100) < 10) this.#modifyEnemy(rndEnemy);
+    return rndEnemy;
+  }
+
+  #modifyEnemy(enemy) {
+    switch (randomInt(0, 3)) {
+      case 0:
+        enemy.color = "hsl(180, 50%, 80%)";
+        enemy.radius = Math.max(Math.ceil(enemy.radius * 0.8), 10);
+        enemy.speed += 1;
+        break;
+      case 1:
+        enemy.color = "hsl(280, 50%, 80%)";
+        enemy.radius = Math.ceil(enemy.radius * 1.25);
+        enemy.hp = enemy.hp + 20;
+        break;
+      case 2:
+        enemy.color = "hsl(40, 80%, 50%)";
+        enemy.radius = Math.ceil(enemy.radius * 1.5);
+        enemy.speed = Math.max(enemy.speed - 1, 1);
+        enemy.hp = enemy.hp + 40;
+        break;
+    }
+    enemy.score = { ...enemy.score };
+    enemy.score.hit *= 2;
+    enemy.score.death *= enemy.hp / 10;
   }
 
   #createEnemy() {
