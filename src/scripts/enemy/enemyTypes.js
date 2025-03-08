069b5c078a00b3522ca89248c017f5f3fe1af77b
Gabriel <gabriel.gracilianomb@gmail.com>
2025-03-08 17:58:10 -0300

    Calculate enemy score dinamically

--- src/scripts/enemy/enemyTypes.js
+++ src/scripts/enemy/enemyTypes.js
@@ -1,44 +1,46 @@
-export const enemyTypes = [
+const enemyTypes = [
   {
     radius: 18,
     speed: 4,
     color: "#ff0000",
     hp: 20,
-    score: { hit: 50, death: 200 },
   },
   {
     radius: 14,
     speed: 5,
     color: "#f210c8",
     hp: 10,
-    score: { hit: 50, death: 200 },
   },
   {
     radius: 25,
     speed: 3,
     color: "#10b2f2",
     hp: 30,
-    score: { hit: 20, death: 100 },
   },
   {
     radius: 20,
     speed: 4,
     color: "#021ffc",
     hp: 30,
-    score: { hit: 30, death: 150 },
   },
   {
     radius: 30,
     speed: 2,
     color: "#1ff40c",
     hp: 50,
-    score: { hit: 50, death: 500 },
   },
   {
     radius: 10,
     speed: 6,
     color: "#fc4d02",
     hp: 10,
-    score: { hit: 50, death: 300 },
   },
 ];
+
+enemyTypes.forEach(t => {
+  t.score = {};
+  t.score.hit = t.hp * t.speed;
+  t.score.death = t.hp * t.speed * 10;
+});
+
+export { enemyTypes };
