65da29f1ac8997a41b687aaa11c4dc6cd0cc7dcb
Gabriel <gabriel.gracilianomb@gmail.com>
2025-03-18 18:04:59 -0300

    Set base in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 
-export default defineConfig ({
+export default defineConfig({
   root: "src",
   publicDir: "../public",
   build: {
-    // Limit of asset size to be transformed into base64
-    // assetsInlineLimit: 2048,
     outDir: "../dist",
-    emptyOutDir: true
-  }
+    emptyOutDir: true,
+  },
+  base: "/2d-canvas-shooter/",
 });
