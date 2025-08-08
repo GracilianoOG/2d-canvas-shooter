b7fe61fbb831d7d9944f67c03ebd0d607b83f87c
Gabriel <gabriel.gracilianomb@gmail.com>
2025-08-08 00:05:48 -0300

    Organize screens and buttons

--- src/scripts/main/main.js
+++ src/scripts/main/main.js
@@ -2,41 +2,41 @@ import { Canvas } from "../Canvas.js";
 import { Game } from "../game/Game.js";
 import { StorageHandler } from "../StorageHandler.js";
 import { CSS_CLASSES } from "../utils/constants.js";
-import { pause, start, restart } from "../utils/screens.js";
+import * as Screens from "../utils/screens.js";
 import "../../styles/main.scss";
 
 const highscoreBoard = document.querySelector(CSS_CLASSES.HIGHSCORE_POINTS);
-const container = document.querySelector("#game-container");
 const pauseBtn = document.querySelector(".pause-btn");
+const restartBtn = document.querySelector(".gameover-screen__btn");
 highscoreBoard.textContent = StorageHandler.retrieveHighscore();
 
 const game = new Game({ width: 800, height: 600 });
 
-start.addEventListener(
+Screens.start.addEventListener(
   "click",
   e => {
     e.stopPropagation();
-    start.classList.add("hide");
-    container.classList.remove("hide");
+    Screens.start.classList.add("hide");
+    Screens.game.classList.remove("hide");
     game.init();
   },
   { once: true }
 );
 
-pause.addEventListener("click", e => {
+Screens.pause.addEventListener("click", e => {
   e.stopPropagation();
   game.pause();
+});
+
+restartBtn.addEventListener("click", e => {
+  e.stopPropagation();
+  Screens.restart.classList.add("hide");
+  game.restart();
 });
 
 pauseBtn.addEventListener("click", e => {
   e.stopPropagation();
   game.pause();
-});
-
-restart.children[1].addEventListener("click", e => {
-  e.stopPropagation();
-  restart.classList.add("hide");
-  game.restart();
 });
 
 window.addEventListener("resize", () =>
