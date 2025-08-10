e2a8ebb420b0454f7bd896ec81b3bf99eefa6ef4
Gabriel <gabriel.gracilianomb@gmail.com>
2025-08-09 23:25:26 -0300

    Create Shotgun

---
+++ src/scripts/arsenal/Shotgun.js
@@ -0,0 +1,24 @@
+import { randomNumber } from "../utils/utility";
+import { Bullet } from "./Bullet";
+import { Weapon } from "./Weapon";
+
+class Shotgun extends Weapon {
+  constructor(owner, cooldown = 200) {
+    super(owner, cooldown);
+  }
+
+  createProjectile() {
+    const { playerX, playerY, bulletAngle } = this._calcBulletPath();
+    const max = 0.2;
+    const min = -max;
+    const color = this.owner.color;
+    const MAX_BULLETS = 3;
+
+    for (let i = 0; i < MAX_BULLETS; i++) {
+      const accuracy = randomNumber(min, max);
+      new Bullet(playerX, playerY, 5, 20, bulletAngle + accuracy, color);
+    }
+  }
+}
+
+export { Shotgun };
