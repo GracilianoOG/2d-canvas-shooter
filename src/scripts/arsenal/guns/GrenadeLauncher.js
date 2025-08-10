c7f2fde1f5e0574555b258731beb5209db23de33
Gabriel <gabriel.gracilianomb@gmail.com>
2025-08-10 00:42:06 -0300

    Create GrenadeLauncher

---
+++ src/scripts/arsenal/guns/GrenadeLauncher.js
@@ -0,0 +1,15 @@
+import { Grenade } from "../ammo/Grenade";
+import { Weapon } from "./Weapon";
+
+class GrenadeLauncher extends Weapon {
+  constructor(owner, cooldown = 220) {
+    super(owner, cooldown);
+  }
+
+  createProjectile() {
+    const { playerX, playerY, bulletAngle } = this._calcBulletPath();
+    new Grenade(playerX, playerY, 6, 15, bulletAngle, this.owner.color, 20);
+  }
+}
+
+export { GrenadeLauncher };
