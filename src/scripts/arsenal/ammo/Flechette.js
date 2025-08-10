1ca83d8846a3cf521fc1e1b95cbcc267952c1031
Gabriel <gabriel.gracilianomb@gmail.com>
2025-08-10 11:20:25 -0300

    Create Flechette ammo

---
+++ src/scripts/arsenal/ammo/Flechette.js
@@ -0,0 +1,49 @@
+import { gameState } from "../../singletons/GameState";
+import { GRAY } from "../../utils/constants/colors";
+import { Bullet } from "../Bullet";
+
+class Flechette extends Bullet {
+  #bounces;
+  #maxBounces;
+
+  constructor(x, y, radius, speed, angle, color, damage = 10, maxBounces = 10) {
+    super(x, y, radius, speed, angle, color, damage);
+    this.#maxBounces = maxBounces;
+    this.#bounces = 0;
+  }
+
+  hasTouchedBorder(canvas = gameState.getEntity("mainCanvas")) {
+    const {
+      x: ballX,
+      y: ballY,
+      dimensions: { radius: radius },
+    } = this;
+
+    const { width: canvasW, height: canvasH } = canvas;
+
+    const LEFT = ballX < radius;
+    const RIGHT = ballX + radius > canvasW;
+    const TOP = ballY < radius;
+    const BOTTOM = ballY + radius > canvasH;
+
+    if (LEFT || RIGHT) {
+      this.angle = Math.PI - this.angle;
+    } else if (TOP || BOTTOM) {
+      this.angle = -this.angle;
+    } else {
+      return;
+    }
+
+    this.#bounces++;
+  }
+
+  onUpdate() {
+    if (this.#bounces >= this.#maxBounces) {
+      this.color = GRAY;
+      return;
+    }
+    this.hasTouchedBorder();
+  }
+}
+
+export { Flechette };
