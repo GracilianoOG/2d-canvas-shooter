95d3ddee6456a6da37767ac9b9a957129555e734
Gabriel <gabriel.gracilianomb@gmail.com>
2025-08-09 23:00:19 -0300

    Create Launcher

---
+++ src/scripts/arsenal/Launcher.js
@@ -0,0 +1,15 @@
+import { Missile } from "./ammo/Missile";
+import { Weapon } from "./Weapon";
+
+class Launcher extends Weapon {
+  constructor(player, cooldown = 300) {
+    super(player, cooldown);
+  }
+
+  createProjectile() {
+    const { playerX, playerY, bulletAngle } = this._calcBulletPath();
+    new Missile(playerX, playerY, 8, 10, bulletAngle, this.owner.color, 20);
+  }
+}
+
+export { Launcher };
