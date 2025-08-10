4d8032195db0b203a2660d1ca62f6469cad4aca3
Gabriel <gabriel.gracilianomb@gmail.com>
2025-08-10 19:55:15 -0300

    Create AcidShotgun

---
+++ src/scripts/arsenal/guns/AcidShotgun.js
@@ -0,0 +1,25 @@
+import { ACID_GREEN } from "../../utils/constants/colors";
+import { randomNumber } from "../../utils/utility";
+import { Bullet } from "../Bullet";
+import { Weapon } from "./Weapon";
+
+class AcidShotgun extends Weapon {
+  constructor(owner, cooldown = 200) {
+    super(owner, cooldown);
+  }
+
+  createProjectile() {
+    const { playerX, playerY, bulletAngle } = this._calcBulletPath();
+    const max = 0.3;
+    const min = -max;
+    const MAX_BULLETS = 3;
+
+    for (let i = 0; i < MAX_BULLETS; i++) {
+      const accuracy = randomNumber(min, max);
+      const angle = bulletAngle + accuracy;
+      new Bullet(playerX, playerY, 5, 20, angle, ACID_GREEN, 20);
+    }
+  }
+}
+
+export { AcidShotgun };
