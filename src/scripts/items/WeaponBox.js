2d82f815506a20d756610d25645b3332c0477a27
Gabriel <gabriel.gracilianomb@gmail.com>
2025-08-11 13:57:47 -0300

    Set default radius and color for weapon box

--- src/scripts/items/WeaponBox.js
+++ src/scripts/items/WeaponBox.js
@@ -1,8 +1,9 @@
 import { eventManager } from "../singletons/EventManager";
+import { YELLOW } from "../utils/constants/colors";
 import { Item } from "./Item";
 
 class WeaponBox extends Item {
-  constructor(x, y, radius, color) {
+  constructor(x, y, radius = 10, color = YELLOW) {
     super(x, y, radius, color);
   }
 
