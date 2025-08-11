7488e9f48713eb2e3d77f29a043a54e62c81655d
Gabriel <gabriel.gracilianomb@gmail.com>
2025-08-11 20:22:01 -0300

    Despawn item it's not collected

--- src/scripts/items/Item.js
+++ src/scripts/items/Item.js
@@ -1,11 +1,21 @@
 import { Entity } from "../Entity";
+import { Timer } from "../Timer";
 
 class Item extends Entity {
-  constructor(x, y, radius, color) {
+  #despawnTimer;
+
+  constructor(x, y, radius, color, despawnTime = 10_000) {
     super(x, y, radius, color);
+    this.#despawnTimer = new Timer(
+      despawnTime,
+      { loop: false, autodestruct: true },
+      () => this.destroy()
+    );
   }
 
-  collect() {}
+  collect() {
+    this.#despawnTimer.remove();
+  }
 }
 
 export { Item };
