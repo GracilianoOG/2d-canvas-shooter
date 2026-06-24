f2948314f93cd12c1c162f5c58bb92bc2a5787b5
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-24 18:48:18 -0300

    Add path alias to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import path from "node:path";
 
 export default defineConfig({
   root: "src",
@@ -8,4 +9,9 @@ export default defineConfig({
     emptyOutDir: true,
   },
   base: "/2d-canvas-shooter/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
