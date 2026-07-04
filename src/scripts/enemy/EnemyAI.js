30ab142091346a77bafa200f8b609dcc086dbeaa
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-04 15:04:18 -0300

    Create EnemyAI

---
+++ src/scripts/enemy/EnemyAI.js
@@ -0,0 +1,20 @@
+class EnemyAI {
+  #enemy;
+
+  constructor(enemy) {
+    this.#enemy = enemy;
+  }
+
+  followTarget(target, delta) {
+    const enemy = this.#enemy;
+    const dirX = target.x - enemy.x;
+    const dirY = target.y - enemy.y;
+    const angle = Math.atan2(dirY, dirX);
+    if (Math.hypot(dirX, dirY) > enemy.dimensions.radius) {
+      enemy.x += Math.cos(angle) * enemy.speed * delta;
+      enemy.y += Math.sin(angle) * enemy.speed * delta;
+    }
+  }
+}
+
+export { EnemyAI };
