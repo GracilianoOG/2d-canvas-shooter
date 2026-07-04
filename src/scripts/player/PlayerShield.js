3eea1623805c1c0fe5ba6bcdb1159308213cb69b
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-04 14:17:04 -0300

    Create PlayerShield

---
+++ src/scripts/player/PlayerShield.js
@@ -0,0 +1,38 @@
+import { Timer } from "../Timer";
+import { defaultStats } from "./playerDefaultStats";
+
+class PlayerShield {
+  #timer;
+
+  constructor(onShieldDepletion) {
+    this.#timer = new Timer(
+      defaultStats.shieldDelay,
+      { autostart: false, loop: false },
+      () => onShieldDepletion(),
+    );
+  }
+
+  activate(delay) {
+    this.#timer.waitTime = delay ?? defaultStats.shieldDelay;
+    this.#timer.reset();
+  }
+
+  reset() {
+    this.#timer.stop();
+    this.#timer.waitTime = defaultStats.shieldDelay;
+  }
+
+  isActive() {
+    return this.#timer.active;
+  }
+
+  get remainingTime() {
+    return this.#timer.elapsedTime;
+  }
+
+  get currentDelay() {
+    return this.#timer.waitTime;
+  }
+}
+
+export { PlayerShield };
