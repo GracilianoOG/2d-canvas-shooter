3309865908747c0134617fb79daa9a146f7b253f
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-05 10:39:07 -0300

    Pass default options to shotgun
    Removed method override and unused import.

--- src/scripts/arsenal/guns/Shotgun.js
+++ src/scripts/arsenal/guns/Shotgun.js
@@ -1,14 +1,14 @@
-import { randomNumber } from "@/scripts/utils/utility";
 import { PistolAmmo } from "../ammo/PistolAmmo";
 import { Gun } from "./Gun";
 
 class Shotgun extends Gun {
-  constructor(ammoType = new PistolAmmo(), cooldown = 200) {
-    super(ammoType, cooldown);
-  }
-
-  createProjectile(x, y) {
-    super.createProjectile(x, y, 3, 0.2);
+  constructor(ammoType = new PistolAmmo(), options = {}) {
+    super(ammoType, {
+      cooldown: 200,
+      bullets: 3,
+      spread: 0.2,
+      ...options,
+    });
   }
 }
 
