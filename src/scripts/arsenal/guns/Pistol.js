0f597e8cd4d15e4fba9263ed29c4fdbedce6befb
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-05 10:38:33 -0300

    Pass default options to pistol

--- src/scripts/arsenal/guns/Pistol.js
+++ src/scripts/arsenal/guns/Pistol.js
@@ -2,8 +2,11 @@ import { PistolAmmo } from "../ammo/PistolAmmo";
 import { Gun } from "./Gun";
 
 class Pistol extends Gun {
-  constructor(ammoType = new PistolAmmo(), cooldown = 150) {
-    super(ammoType, cooldown);
+  constructor(ammoType = new PistolAmmo(), options = {}) {
+    super(ammoType, {
+      cooldown: 150,
+      ...options,
+    });
   }
 }
 
