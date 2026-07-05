7e01210af0a3e95b5951c2c0ea1c8851cd92788e
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-05 10:47:54 -0300

    Change minigun to use new Gun class

--- src/scripts/arsenal/guns/Minigun.js
+++ src/scripts/arsenal/guns/Minigun.js
@@ -1,20 +1,13 @@
-import { VERY_LIGHT_YELLOW } from "../../utils/constants/colors";
-import { randomNumber } from "../../utils/utility";
-import { Bullet } from "../Bullet";
-import { Weapon } from "./Weapon";
+import { HeavyAmmo } from "../ammo/HeavyAmmo";
+import { Gun } from "./Gun";
 
-class Minigun extends Weapon {
-  constructor(player, cooldown = 90) {
-    super(player, cooldown);
-  }
-
-  createProjectile() {
-    const { playerX, playerY, bulletAngle } = this._calcBulletPath();
-    const max = 0.1;
-    const min = -max;
-    const accuracy = randomNumber(min, max);
-    const color = VERY_LIGHT_YELLOW;
-    new Bullet(playerX, playerY, 5, 1250, bulletAngle - accuracy, color, 20);
+class Minigun extends Gun {
+  constructor(ammoType = new HeavyAmmo(), options = {}) {
+    super(ammoType, {
+      cooldown: 90,
+      spread: 0.1,
+      ...options,
+    });
   }
 }
 
