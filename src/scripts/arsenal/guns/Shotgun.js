beeb71e513a802a4829dbb8221c3b3bda81d0703
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-05 09:34:21 -0300

    Extend Gun on Shotgun

--- src/scripts/arsenal/guns/Shotgun.js
+++ src/scripts/arsenal/guns/Shotgun.js
@@ -1,22 +1,20 @@
-import { randomNumber } from "../../utils/utility";
-import { Bullet } from "../Bullet";
-import { Weapon } from "./Weapon";
+import { randomNumber } from "@/scripts/utils/utility";
+import { PistolAmmo } from "../ammo/PistolAmmo";
+import { Gun } from "./Gun";
 
-class Shotgun extends Weapon {
-  constructor(player, cooldown = 200) {
-    super(player, cooldown);
+class Shotgun extends Gun {
+  constructor(ammoType = new PistolAmmo(), cooldown = 200) {
+    super(ammoType, cooldown);
   }
 
-  createProjectile() {
-    const { playerX, playerY, bulletAngle } = this._calcBulletPath();
-    const max = 0.2;
-    const min = -max;
-    const color = this.player.color;
-    const MAX_BULLETS = 3;
+  createProjectile(x, y) {
+    const { originX, originY, bulletAngle } = this.calcBulletPath(x, y);
+    const spread = 0.2;
+    const bullets = 3;
 
-    for (let i = 0; i < MAX_BULLETS; i++) {
-      const accuracy = randomNumber(min, max);
-      new Bullet(playerX, playerY, 5, 1250, bulletAngle + accuracy, color);
+    for (let i = 0; i < bullets; i++) {
+      const accuracy = randomNumber(spread, -spread);
+      this.ammoType.create(originX, originY, bulletAngle + accuracy);
     }
   }
 }
