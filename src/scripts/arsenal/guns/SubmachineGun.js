3dcbeee426eeaab9640390dd11756253a5423544
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-08 10:47:12 -0300

    Update SubmachineGun to use new object parameter

--- src/scripts/arsenal/guns/SubmachineGun.js
+++ src/scripts/arsenal/guns/SubmachineGun.js
@@ -2,11 +2,19 @@ import { PistolAmmo } from "../ammo/PistolAmmo";
 import { Gun } from "./Gun";
 
 class SubmachineGun extends Gun {
-  constructor(ammoType = new PistolAmmo(), options = {}) {
-    super(ammoType, {
-      cooldown: 120,
-      spread: 0.05,
-      ...options,
+  constructor({
+    name = "SMG",
+    ammoType = new PistolAmmo(),
+    options = {},
+  } = {}) {
+    super({
+      name,
+      ammoType,
+      options: {
+        cooldown: 120,
+        spread: 0.05,
+        ...options,
+      },
     });
   }
 }
