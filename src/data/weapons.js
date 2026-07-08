fbc12e90deb487cd20fab8b6a4e17503a9f432c6
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-08 10:44:49 -0300

    Add ritle to weapon pool

--- src/data/weapons.js
+++ src/data/weapons.js
@@ -1,4 +1,5 @@
 import { Minigun } from "@/scripts/arsenal/guns/Minigun";
+import { Rifle } from "@/scripts/arsenal/guns/Rifle";
 import { Shotgun } from "@/scripts/arsenal/guns/Shotgun";
 import { SubmachineGun } from "@/scripts/arsenal/guns/SubmachineGun";
 import { randomInt } from "@/scripts/utils/utility";
@@ -7,6 +8,7 @@ const weapons = {
   minigun: () => new Minigun(),
   shotgun: () => new Shotgun(),
   smg: () => new SubmachineGun(),
+  rifle: () => new Rifle(),
 };
 
 const weaponList = Object.entries(weapons);
