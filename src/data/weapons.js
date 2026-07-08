4fb5fdb5f3857fb490d3ee9a0f072e960c3ba883
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-08 12:04:44 -0300

    Add ricochet shotgun to weapon pool

--- src/data/weapons.js
+++ src/data/weapons.js
@@ -1,5 +1,6 @@
 import { HeavyShotgun } from "@/scripts/arsenal/guns/HeavyShotgun";
 import { Minigun } from "@/scripts/arsenal/guns/Minigun";
+import { RicochetShotgun } from "@/scripts/arsenal/guns/RicochetShotgun";
 import { Rifle } from "@/scripts/arsenal/guns/Rifle";
 import { Shotgun } from "@/scripts/arsenal/guns/Shotgun";
 import { SubmachineGun } from "@/scripts/arsenal/guns/SubmachineGun";
@@ -9,6 +10,7 @@ const weapons = {
   minigun: () => new Minigun(),
   shotgun: () => new Shotgun(),
   heavyShotgun: () => new HeavyShotgun(),
+  ricochetShotgun: () => new RicochetShotgun(),
   smg: () => new SubmachineGun(),
   rifle: () => new Rifle(),
 };
