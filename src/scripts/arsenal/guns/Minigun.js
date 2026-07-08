f23f98ed7ea3a8747cc868846199be4d8dfab3fd
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-08 10:38:39 -0300

    Change Minigun to use new object parameter

--- src/scripts/arsenal/guns/Minigun.js
+++ src/scripts/arsenal/guns/Minigun.js
@@ -2,11 +2,19 @@ import { HeavyAmmo } from "../ammo/HeavyAmmo";
 import { Gun } from "./Gun";
 
 class Minigun extends Gun {
-  constructor(name = "Minigun", ammoType = new HeavyAmmo(), options = {}) {
-    super(name, ammoType, {
-      cooldown: 90,
-      spread: 0.1,
-      ...options,
+  constructor({
+    name = "Minigun",
+    ammoType = new HeavyAmmo(),
+    options = {},
+  } = {}) {
+    super({
+      name,
+      ammoType,
+      options: {
+        cooldown: 90,
+        spread: 0.1,
+        ...options,
+      },
     });
   }
 }
