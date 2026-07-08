f949364238e4cd371127451cbaeb68e75dc0a493
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-08 10:30:25 -0300

    Change Pistol to use new parameter system

--- src/scripts/arsenal/guns/Pistol.js
+++ src/scripts/arsenal/guns/Pistol.js
@@ -2,10 +2,18 @@ import { PistolAmmo } from "../ammo/PistolAmmo";
 import { Gun } from "./Gun";
 
 class Pistol extends Gun {
-  constructor(name = "Pistol", ammoType = new PistolAmmo(), options = {}) {
-    super(name, ammoType, {
-      cooldown: 150,
-      ...options,
+  constructor({
+    name = "Pistol",
+    ammoType = new PistolAmmo(),
+    options = {},
+  } = {}) {
+    super({
+      name,
+      ammoType,
+      options: {
+        cooldown: 150,
+        ...options,
+      },
     });
   }
 }
