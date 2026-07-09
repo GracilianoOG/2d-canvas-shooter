918a411243fe95a8e9f09b11f1db0c9739ac5e0c
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-09 18:50:58 -0300

    Check if speed is not zero to set it

--- src/scripts/arsenal/ammo/Mine.js
+++ src/scripts/arsenal/ammo/Mine.js
@@ -7,7 +7,9 @@ class Mine extends Explosive {
 
   update(delta) {
     const DEACCELERATION = 1000 * delta;
-    this.speed = Math.max(this.speed - DEACCELERATION, 0);
+    if (this.speed >= 0) {
+      this.speed = Math.max(this.speed - DEACCELERATION, 0);
+    }
     super.update(delta);
   }
 }
