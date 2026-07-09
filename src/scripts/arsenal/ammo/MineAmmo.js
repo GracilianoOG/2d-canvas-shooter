82718876a0bd1b563be14b216ebb0425dcc58b29
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-09 18:39:05 -0300

    Create MineAmmo to create mines

---
+++ src/scripts/arsenal/ammo/MineAmmo.js
@@ -0,0 +1,26 @@
+import { ARMY_GREEN } from "@/scripts/utils/constants/colors";
+import { PistolAmmo } from "./PistolAmmo";
+import { ExplosiveAmmo } from "./ExplosiveAmmo";
+import { HEAVY_DMG } from "./damages";
+import { Mine } from "./Mine";
+
+class MineAmmo extends ExplosiveAmmo {
+  constructor(name = "Mine", fragmentType = new PistolAmmo()) {
+    super(name, fragmentType);
+  }
+
+  create(x, y, angle) {
+    const radius = 6;
+    const speed = 500;
+    const color = ARMY_GREEN;
+    const damage = HEAVY_DMG;
+    const fragments = {
+      amount: 16,
+      createFragments: (x, y, direction) =>
+        this.fragmentType.create(x, y, direction),
+    };
+    new Mine(x, y, radius, speed, angle, color, damage, fragments);
+  }
+}
+
+export { MineAmmo };
