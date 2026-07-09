96bc615dcc76fa378cdd533cf52b4b8a494e3bec
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-09 18:28:59 -0300

    Clean Mine constructor
    Removed unneeded configuration properties.

--- src/scripts/arsenal/ammo/Mine.js
+++ src/scripts/arsenal/ammo/Mine.js
@@ -1,18 +1,8 @@
-import { WHITE } from "../../utils/constants/colors";
 import { Explosive } from "./Explosive";
 
-const defaultOptions = {
-  fragments: 20,
-  size: 6,
-  speed: 500,
-  damage: 10,
-  color: WHITE,
-};
-
 class Mine extends Explosive {
-  constructor(x, y, radius, speed, angle, color, damage = 30, options = {}) {
-    const config = { ...defaultOptions, ...options };
-    super(x, y, radius, speed, angle, color, damage, config);
+  constructor(x, y, radius, speed, angle, color, damage, fragments = {}) {
+    super(x, y, radius, speed, angle, color, damage, fragments);
   }
 
   update(delta) {
