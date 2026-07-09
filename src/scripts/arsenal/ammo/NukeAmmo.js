10f226f0e8e400e96d77b2a586a41b653975fd90
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-09 17:59:00 -0300

    Create NukeAmmo to generate massive explosives

---
+++ src/scripts/arsenal/ammo/NukeAmmo.js
@@ -0,0 +1,26 @@
+import { ARMY_GREEN } from "@/scripts/utils/constants/colors";
+import { ExplosiveAmmo } from "./ExplosiveAmmo";
+import { Explosive } from "./Explosive";
+import { MissileAmmo } from "./MissileAmmo";
+import { CANNON_DMG } from "./damages";
+
+class NukeAmmo extends ExplosiveAmmo {
+  constructor(name = "Nuke", fragmentType = new MissileAmmo()) {
+    super(name, fragmentType);
+  }
+
+  create(x, y, angle) {
+    const radius = 20;
+    const speed = 350;
+    const color = ARMY_GREEN;
+    const damage = CANNON_DMG;
+    const fragments = {
+      amount: 10,
+      createFragments: (x, y, direction) =>
+        this.fragmentType.create(x, y, direction),
+    };
+    new Explosive(x, y, radius, speed, angle, color, damage, fragments);
+  }
+}
+
+export { NukeAmmo };
