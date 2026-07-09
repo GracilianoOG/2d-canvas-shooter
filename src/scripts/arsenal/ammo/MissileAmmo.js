b128ec8a122504890575ada1c390577108012d5b
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-09 17:55:41 -0300

    Create MissileAmmo to create greater explosives

---
+++ src/scripts/arsenal/ammo/MissileAmmo.js
@@ -0,0 +1,26 @@
+import { ARMY_GREEN } from "@/scripts/utils/constants/colors";
+import { PistolAmmo } from "./PistolAmmo";
+import { ExplosiveAmmo } from "./ExplosiveAmmo";
+import { Explosive } from "./Explosive";
+import { HEAVY_DMG } from "./damages";
+
+class MissileAmmo extends ExplosiveAmmo {
+  constructor(name = "Missile", fragmentType = new PistolAmmo()) {
+    super(name, fragmentType);
+  }
+
+  create(x, y, angle) {
+    const radius = 12;
+    const speed = 400;
+    const color = ARMY_GREEN;
+    const damage = HEAVY_DMG;
+    const fragments = {
+      amount: 20,
+      createFragments: (x, y, direction) =>
+        this.fragmentType.create(x, y, direction),
+    };
+    new Explosive(x, y, radius, speed, angle, color, damage, fragments);
+  }
+}
+
+export { MissileAmmo };
