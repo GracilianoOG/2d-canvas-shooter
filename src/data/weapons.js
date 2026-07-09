c896476d84afaf0f8b9815b7b4c7b1216252790e
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-09 18:13:58 -0300

    Add grenade launcher to weapon list

--- src/data/weapons.js
+++ src/data/weapons.js
@@ -1,5 +1,6 @@
 import { BulletHell } from "@/scripts/arsenal/guns/BulletHell";
 import { Cannon } from "@/scripts/arsenal/guns/Cannon";
+import { GrenadeLauncher } from "@/scripts/arsenal/guns/GrenadeLauncher";
 import { HeavyShotgun } from "@/scripts/arsenal/guns/HeavyShotgun";
 import { Minigun } from "@/scripts/arsenal/guns/Minigun";
 import { NukeLauncher } from "@/scripts/arsenal/guns/NukeLauncher";
@@ -19,6 +20,7 @@ const weapons = {
   hell: () => new BulletHell(),
   cannon: () => new Cannon(),
   nuke: () => new NukeLauncher(),
+  launcher: () => new GrenadeLauncher(),
 };
 
 const weaponList = Object.entries(weapons);
