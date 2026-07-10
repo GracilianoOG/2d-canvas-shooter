40318c8d2c085e245aba10063dc1d2c7c807dc03
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-10 15:14:49 -0300

    Create gains to each sound type
    Created gain to master, sound effects, and to music.

--- src/scripts/audio/AudioSystem.js
+++ src/scripts/audio/AudioSystem.js
@@ -13,10 +13,26 @@ import battleMp3 from "/assets/audios/music/battle-loop.mp3";
 class AudioSystem {
   #audios;
   #context;
+  #master;
+  #sfx;
+  #music;
 
   constructor() {
     this.#audios = new Map();
     this.#context = new AudioContext();
+
+    this.#sfx = this.#context.createGain();
+    this.#music = this.#context.createGain();
+    this.#master = this.#context.createGain();
+
+    this.#sfx.connect(this.#master);
+    this.#music.connect(this.#master);
+    this.#master.connect(this.#context.destination);
+
+    this.#master.gain.value = 1;
+    this.#sfx.gain.value = 0.5;
+    this.#music.gain.value = 0.8;
+
     this.load("hit", hitOgg);
     this.load("explosion", explosionOgg);
     this.load("shot", laserOgg);
@@ -35,7 +51,7 @@ class AudioSystem {
     const source = this.#context.createBufferSource();
     const audioBuffer = this.#audios.get(name);
     source.buffer = audioBuffer;
-    source.connect(this.#context.destination);
+    source.connect(this.#sfx);
     source.start();
   }
 }
