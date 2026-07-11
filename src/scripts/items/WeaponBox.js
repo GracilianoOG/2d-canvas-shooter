72e8edd0f9d8daf3af44ee262d6c2a2034778dad
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-11 20:56:37 -0300

    Change weapon box color to CHARTREUSE

--- src/scripts/items/WeaponBox.js
+++ src/scripts/items/WeaponBox.js
@@ -1,10 +1,10 @@
 import { getRandomWeapon } from "@/data/weapons";
 import { eventManager } from "../singletons/EventManager";
-import { YELLOW } from "../utils/constants/colors";
+import { CHARTREUSE } from "../utils/constants/colors";
 import { Item } from "./Item";
 
 class WeaponBox extends Item {
-  constructor(x, y, radius = 10, color = YELLOW, label = "gun") {
+  constructor(x, y, radius = 10, color = CHARTREUSE, label = "gun") {
     super(x, y, radius, color, label);
   }
 
