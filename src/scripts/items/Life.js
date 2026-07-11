50f564e5b280bec857ebde438dd238113e3aaa07
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-11 20:57:27 -0300

    Change life color to CRIMSON

--- src/scripts/items/Life.js
+++ src/scripts/items/Life.js
@@ -1,9 +1,9 @@
 import { eventManager } from "../singletons/EventManager";
-import { LIGHT_RED } from "../utils/constants/colors";
+import { CRIMSON } from "../utils/constants/colors";
 import { Item } from "./Item";
 
 class Life extends Item {
-  constructor(x, y, radius = 10, color = LIGHT_RED, label = "life") {
+  constructor(x, y, radius = 10, color = CRIMSON, label = "life") {
     super(x, y, radius, color, label);
   }
 
