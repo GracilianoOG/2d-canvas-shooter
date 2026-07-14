59775efd8072d7dec79bad47a3bbce61982059ae
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-14 16:31:32 -0300

    Remove randomLinePoint

--- src/scripts/utils/utility.js
+++ src/scripts/utils/utility.js
@@ -1,5 +1,2 @@
 export const minOrMaxPoint = (minPoint, maxPoint) =>
   Math.random() > 0.5 ? minPoint : maxPoint;
-
-export const randomLinePoint = (lineWidth) =>
-  Math.floor(Math.random() * lineWidth);
