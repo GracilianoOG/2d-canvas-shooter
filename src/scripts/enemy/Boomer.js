76e89f5f9c49caf5ad78de04cc962f7038c8cf47
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-16 20:59:33 -0300

    Create Boomer enemy
    This enemy spawn several minions on death.

---
+++ src/scripts/enemy/Boomer.js
@@ -0,0 +1,47 @@
+import { randomInt } from "../utils/math";
+import { Enemy } from "./Enemy";
+
+class Boomer extends Enemy {
+  #baseSize;
+  #baseSpeed;
+  #baseHp;
+  #target;
+
+  constructor(x, y, radius, speed, color, health, score, target, options = {}) {
+    super(x, y, radius, speed, color, health, score, target, options);
+    this.#target = target;
+    this.#baseSize = radius;
+    this.#baseSpeed = speed;
+    this.#baseHp = health;
+  }
+
+  onDestroy() {
+    const amount = randomInt(5, 2);
+    for (let i = 0; i < amount; i++) {
+      new Enemy(
+        this.x + randomInt(this.#baseSize, -this.#baseSize),
+        this.y + randomInt(this.#baseSize, -this.#baseSize),
+        Math.max(this.#baseSize / 2, 10),
+        Math.max(Math.floor(this.#baseSpeed / 2), 5),
+        this.baseColor,
+        Math.max(this.#baseHp / 2, 10),
+        this.score,
+        this.#target,
+      );
+    }
+    this.drop(0.05);
+  }
+
+  draw(ctx) {
+    super.draw(ctx);
+    ctx.save();
+    ctx.fillStyle = this.color;
+    ctx.globalCompositeOperation = "lighter";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius * 0.8, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.restore();
+  }
+}
+
+export { Boomer };
