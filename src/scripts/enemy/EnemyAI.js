c979ae27e68c27b6b791357219209e9099e5f808
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-15 22:16:30 -0300

    Remove dimensions from EnemyAI

--- src/scripts/enemy/EnemyAI.js
+++ src/scripts/enemy/EnemyAI.js
@@ -10,7 +10,7 @@ class EnemyAI {
     const dirX = target.x - enemy.x;
     const dirY = target.y - enemy.y;
     const angle = Math.atan2(dirY, dirX);
-    if (Math.hypot(dirX, dirY) > enemy.dimensions.radius) {
+    if (Math.hypot(dirX, dirY) > enemy.radius) {
       enemy.x += Math.cos(angle) * enemy.speed * delta;
       enemy.y += Math.sin(angle) * enemy.speed * delta;
     }
