a069af13bc763649f9bb54a6f153c96c00c1ceee
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-16 19:55:17 -0300

    Remove unneeded constructor from Mine

--- src/scripts/arsenal/projectiles/Mine.js
+++ src/scripts/arsenal/projectiles/Mine.js
@@ -1,10 +1,6 @@
 import { Explosive } from "./Explosive";
 
 class Mine extends Explosive {
-  constructor(x, y, radius, speed, angle, color, damage, fragments = {}) {
-    super(x, y, radius, speed, angle, color, damage, fragments);
-  }
-
   update(delta) {
     const DEACCELERATION = 1000 * delta;
     if (this.speed >= 0) {
