21fddd6de46881d6c1030772b04d1d96b018363b
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-17 15:04:03 -0300

    Create Cloaker enemy
    This enemy starts opaque and slowly becomes transparent.

---
+++ src/scripts/enemy/Cloaker.js
@@ -0,0 +1,29 @@
+import { Enemy } from "./Enemy";
+
+const LIMIT = 5;
+const THRESHOLD = 30;
+
+class Cloaker extends Enemy {
+  #opacity = THRESHOLD;
+
+  takeDamage(damage) {
+    super.takeDamage(damage);
+    this.#opacity = THRESHOLD;
+  }
+
+  draw(ctx) {
+    ctx.save();
+    ctx.globalAlpha = this.#opacity / 100;
+    super.draw(ctx);
+    ctx.restore();
+  }
+
+  update(delta) {
+    super.update(delta);
+    if (this.#opacity > LIMIT) {
+      this.#opacity = Math.max(this.#opacity - 0.1, LIMIT);
+    }
+  }
+}
+
+export { Cloaker };
