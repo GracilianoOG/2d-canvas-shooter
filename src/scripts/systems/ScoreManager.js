03ed24a2fae9e7496c97a12a5c755b5216f666b5
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-19 10:06:40 -0300

    Create ScoreManager

---
+++ src/scripts/systems/ScoreManager.js
@@ -0,0 +1,39 @@
+import { eventManager } from "../singletons/EventManager";
+import { StorageHandler } from "../StorageHandler";
+
+class ScoreManager {
+  #score;
+
+  constructor() {
+    this.#score = 0;
+  }
+
+  get score() {
+    return this.#score;
+  }
+
+  get highscore() {
+    return parseInt(StorageHandler.retrieveHighscore(false));
+  }
+
+  add(score) {
+    this.#score += score;
+    eventManager.emit("addScore", { score: this.#score });
+  }
+
+  isHighscore() {
+    return this.score > this.highscore;
+  }
+
+  save() {
+    if (this.isHighscore()) {
+      StorageHandler.storeHighscore(this.score);
+    }
+  }
+
+  reset() {
+    this.#score = 0;
+  }
+}
+
+export const scoreManager = new ScoreManager();
