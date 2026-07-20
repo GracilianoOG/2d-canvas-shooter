e3abe25ffe7570ab500115f03a981c9c8afc4fd6
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-20 14:43:01 -0300

    Extend Bar component

--- src/game/ui/FuryMeter.js
+++ src/game/ui/FuryMeter.js
@@ -1,31 +1,9 @@
 import { eventManager } from "../../engine/systems/EventManager";
-import { clamp } from "../../engine/utils/math";
+import { Bar } from "../../engine/ui/Bar";
 
-const MAX_FURY_VALUE = 100;
-
-class FuryMeter {
-  #furyMeterBar;
-  #furyMeterFill;
-  #furyLabel;
-  #furyValue;
-
-  constructor(containerEl) {
-    this.#furyMeterBar = document.createElement("div");
-    this.#furyMeterBar.classList.add("fury-meter");
-
-    this.#furyMeterFill = document.createElement("div");
-    this.#furyMeterFill.classList.add("fury-meter__fill");
-
-    this.#furyLabel = document.createElement("span");
-    this.#furyLabel.textContent = "fury";
-    this.#furyLabel.classList.add("fury-meter__text");
-
-    this.#furyMeterBar.append(this.#furyMeterFill);
-    this.#furyMeterBar.append(this.#furyLabel);
-
-    containerEl.append(this.#furyMeterBar);
-
-    this.#furyValue = 0;
+export class FuryMeter extends Bar {
+  constructor(args) {
+    super(args);
 
     eventManager.subscribe("fillFuryMeter", ({ amount }) => this.fill(amount));
     eventManager.subscribe("checkFuryMeterToFill", ({ collect, amount }) => {
@@ -42,8 +20,7 @@ class FuryMeter {
   }
 
   #onEmptyFuryMeter({ timePerc }) {
-    this.#furyValue = timePerc * MAX_FURY_VALUE;
-    this.#updateFuryMeter();
+    this.value = timePerc * this.maxValue;
   }
 
   #onFuryActivation() {
@@ -51,43 +28,4 @@ class FuryMeter {
       eventManager.emit("activateFury");
     }
   }
-
-  #updateFuryMeter() {
-    this.#furyMeterFill.style.width = `${this.#furyValue}%`;
-
-    this.#furyMeterBar.classList.toggle("fury-meter--full", this.isFull());
-    this.#furyMeterBar.classList.toggle(
-      "fury-meter--filling",
-      this.isFilling(),
-    );
-    this.#furyMeterFill.classList.toggle(
-      "fury-meter__fill--full",
-      this.isFull(),
-    );
-  }
-
-  fill(value) {
-    this.#furyValue = clamp(0, this.#furyValue + value, MAX_FURY_VALUE);
-    this.#updateFuryMeter();
-  }
-
-  unfill(value) {
-    this.#furyValue = clamp(0, this.#furyValue - value, MAX_FURY_VALUE);
-    this.#updateFuryMeter();
-  }
-
-  empty() {
-    this.#furyValue = 0;
-    this.#updateFuryMeter();
-  }
-
-  isFilling() {
-    return this.#furyValue > 0 && this.#furyValue < this.isFull();
-  }
-
-  isFull() {
-    return this.#furyValue === MAX_FURY_VALUE;
-  }
 }
-
-export { FuryMeter };
