2629ddb67712e574a0f2c0d7cad3dff5f38766ed
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-22 10:53:48 -0300

    Remove target fps

--- src/engine/core/Engine.js
+++ src/engine/core/Engine.js
@@ -6,7 +6,6 @@ export class Engine {
   #lastTime;
   #state;
   #shaker;
-  #TARGET_FPS;
 
   constructor(update, render, configs) {
     this.update = update;
@@ -16,8 +15,6 @@ export class Engine {
     this.#lastTime = 0;
     this.#state = States.NOT_RUNNING;
     this.#shaker = new Shaker(configs.ctx);
-
-    this.#TARGET_FPS = 1000 / (configs?.FPS ?? 60);
   }
 
   get shaker() {
