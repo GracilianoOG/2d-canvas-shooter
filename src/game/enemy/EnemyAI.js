07264363bcdede4c24af7f51eeacd0ea7a8b4764
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-22 23:33:03 -0300

    Refactor followTarget method

--- src/game/enemy/EnemyAI.js
+++ src/game/enemy/EnemyAI.js
@@ -7,10 +7,9 @@ class EnemyAI {
 
   followTarget(target, delta) {
     const enemy = this.#enemy;
-    const dirX = target.x - enemy.x;
-    const dirY = target.y - enemy.y;
-    const angle = Math.atan2(dirY, dirX);
-    if (Math.hypot(dirX, dirY) > enemy.radius) {
+    const position = { x: target.x, y: target.y };
+    const angle = enemy.angleTo(position);
+    if (enemy.distanceTo(position) > enemy.radius) {
       enemy.x += Math.cos(angle) * enemy.speed * delta;
       enemy.y += Math.sin(angle) * enemy.speed * delta;
     }
