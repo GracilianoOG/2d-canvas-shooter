fbe379a9ae0b5b23974d075520c6244439444796
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-22 23:43:25 -0300

    Change sentry color to powder blue

--- src/game/entities/items/SentryBox.js
+++ src/game/entities/items/SentryBox.js
@@ -1,15 +1,15 @@
-import { WHITE } from "@/game/utils/constants/colors";
+import { POWDER_BLUE } from "@/game/utils/constants/colors";
 import { Item } from "./Item";
 import { entityManager } from "@/game/systems/EntityManager";
 import { Sentry } from "../Sentry";
 
 export class SentryBox extends Item {
-  constructor(x, y, radius, color = WHITE, label = "sentry") {
+  constructor(x, y, radius, color = POWDER_BLUE, label = "sentry") {
     super(x, y, radius, color, label);
   }
 
   check() {
-    const turret = new Sentry(this.x, this.y, 12, "#B0E0E6");
+    const turret = new Sentry(this.x, this.y, 12, POWDER_BLUE);
     entityManager.add(turret);
     this.collect();
   }
