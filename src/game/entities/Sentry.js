1aea2d2d4f2ff75ae5d9e546085421cd0a620552
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-22 22:46:38 -0300

    Create Sentry
    The sentry will shoot at any enemy that may get close enough.

---
+++ src/game/entities/Sentry.js
@@ -0,0 +1,81 @@
+import { Timer } from "@/engine/systems/Timer";
+import { PistolAmmo } from "../arsenal/ammo/PistolAmmo";
+import { entityManager } from "../systems/EntityManager";
+import { Entity } from "./Entity";
+import { gameState } from "../core/GameState";
+
+export class Sentry extends Entity {
+  #target;
+  #ammoType;
+  #range;
+  #cooldown;
+  #despawnTimer;
+
+  constructor(x, y, radius, color, range = 200, duration = 20_000) {
+    super(x, y, radius, color);
+    this.#target = null;
+    this.#ammoType = new PistolAmmo();
+    this.#range = range;
+    this.#cooldown = new Timer(150, { loop: false });
+    this.#despawnTimer = new Timer(duration, { autodestruct: true }, () =>
+      this.destroy(),
+    );
+  }
+
+  #traceTarget() {
+    const dirX = this.#target.x - this.x;
+    const dirY = this.#target.y - this.y;
+    const bulletAngle = Math.atan2(dirY, dirX);
+
+    return bulletAngle;
+  }
+
+  #shoot() {
+    if (this.#cooldown.active) return;
+    this.#cooldown.reset();
+    const direction = this.#traceTarget();
+    this.#ammoType.create(this.x, this.y, direction);
+    gameState.getEntity("gameAudio").play("shot");
+  }
+
+  #drawDespawnDelay(ctx) {
+    const { waitTime: despawnDelay } = this.#despawnTimer;
+    const { elapsedTime } = this.#despawnTimer;
+    const timePerc = elapsedTime / despawnDelay;
+    const padding = 3;
+
+    this.drawArc(ctx, this.color, padding, timePerc);
+  }
+
+  draw(ctx) {
+    super.draw(ctx);
+    this.#drawDespawnDelay(ctx);
+  }
+
+  update() {
+    if (this.#target) {
+      this.#shoot();
+
+      const dirX = this.#target.x - this.x;
+      const dirY = this.#target.y - this.y;
+
+      if (this.#target.destroyed || Math.hypot(dirX, dirY) > this.#range) {
+        this.#target = null;
+      } else {
+        return;
+      }
+    }
+
+    const enemies = entityManager.entities.filter((ent) => ent?.drop);
+
+    for (const enemy of enemies) {
+      const dirX = enemy.x - this.x;
+      const dirY = enemy.y - this.y;
+
+      if (Math.hypot(dirX, dirY) < this.#range) {
+        this.#target = enemy;
+        return;
+      }
+    }
+  }
+}
