efc865821a1e20c4a1fafb6c05004f59bcee418d
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-25 20:30:48 -0300

    Create Shards item

---
+++ src/game/entities/items/Shards.js
@@ -0,0 +1,14 @@
+import { WHITE } from "@/game/utils/constants/colors";
+import { Item } from "./Item";
+import { eventManager } from "@/engine/systems/EventManager";
+
+export class Shards extends Item {
+  constructor(x, y, radius, color = WHITE, label = "shards") {
+    super(x, y, radius, color, label);
+  }
+
+  check() {
+    eventManager.emit("shardsCollected");
+    this.collect();
+  }
+}
