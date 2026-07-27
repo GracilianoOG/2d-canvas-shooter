bb9b2ebbd6811170bdd43534c7584b464409f44f
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-27 11:08:17 -0300

    Stop calculating mine speed if it is 0

--- src/game/entities/projectiles/Mine.js
+++ src/game/entities/projectiles/Mine.js
@@ -3,7 +3,7 @@ import { Explosive } from "./Explosive";
 class Mine extends Explosive {
   update(delta) {
     const DEACCELERATION = 1000 * delta;
-    if (this.speed >= 0) {
+    if (this.speed > 0) {
       this.speed = Math.max(this.speed - DEACCELERATION, 0);
     }
     super.update(delta);
