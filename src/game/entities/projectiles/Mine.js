90b50ec10c5f54a1490f16c673d46df01d99ddec
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-27 11:13:03 -0300

    Move DEACCELERATION into if

--- src/game/entities/projectiles/Mine.js
+++ src/game/entities/projectiles/Mine.js
@@ -2,8 +2,8 @@ import { Explosive } from "./Explosive";
 
 class Mine extends Explosive {
   update(delta) {
-    const DEACCELERATION = 1000 * delta;
     if (this.speed > 0) {
+      const DEACCELERATION = 1000 * delta;
       this.speed = Math.max(this.speed - DEACCELERATION, 0);
     }
     super.update(delta);
