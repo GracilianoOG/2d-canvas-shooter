df899a04a462117891e213eff4aa90bc07feac99
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-29 16:00:03 -0300

    Reduce chance to drop Nuke item

--- src/game/items/itemDrop.js
+++ src/game/items/itemDrop.js
@@ -7,13 +7,13 @@ import { Shield } from "../entities/items/Shield";
 import { WeaponBox } from "../entities/items/WeaponBox";
 
 const dropTable = [
+  [(x, y) => new Nuke(x, y), 5],
   [(x, y) => new Life(x, y), 10],
   [(x, y) => new Shards(x, y), 20],
   [(x, y) => new SentryBox(x, y), 20],
   [(x, y) => new Shield(x, y), 30],
   [(x, y) => new Adrenaline(x, y), 60],
   [(x, y) => new WeaponBox(x, y), 100],
-  [(x, y) => new Nuke(x, y), 10000],
 ];
 
 export const dropRandomItem = (x, y, chance = 0.1) => {
