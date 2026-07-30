d6fa5a6bebf11863fbbd1c6e8eba6b3f7dc592df
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-30 12:39:58 -0300

    Set pistol as default weapon to equipped

--- src/game/player/PlayerArsenal.js
+++ src/game/player/PlayerArsenal.js
@@ -16,7 +16,7 @@ export class PlayerArsenal {
   constructor(player) {
     this.#player = player;
     this.#inventory = weapons;
-    this.#equipDefault();
+    this.#equipped = this.#get("pistol");
 
     this.#duration = 12_000;
     this.#timer = Timer.create(
