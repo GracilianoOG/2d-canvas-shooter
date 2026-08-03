d73ed3a792404a02bf09958e879568b31a98dedf
Gabriel <gabriel.gracilianomb@gmail.com>
2026-08-02 21:26:49 -0300

    Delete screens file

--- src/game/utils/screens.js
+++
@@ -1,5 +0,0 @@
-export const game = document.querySelector("#game-container");
-export const start = document.querySelector(".start-screen");
-export const pause = document.querySelector(".pause-screen");
-export const restart = document.querySelector(".gameover-screen");
-export const loading = document.querySelector(".loading-screen");
