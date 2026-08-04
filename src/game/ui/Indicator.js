0b6e27dce9e6211ee7bb5da061a991e184393520
Gabriel <gabriel.gracilianomb@gmail.com>
2026-08-04 13:39:36 -0300

    Remove screen factors from Indicator

--- src/game/ui/Indicator.js
+++ src/game/ui/Indicator.js
@@ -1,15 +1,12 @@
-import { gameState } from "../core/GameState";
-
 export class Indicator {
   static create(position, text, color = "#fff") {
     const indicator = document.createElement("div");
     const container = document.querySelector("#status-container");
-    const factors = gameState.getEntity("mainCanvas").factors;
 
     indicator.setAttribute("class", "indicator");
     indicator.textContent = text;
-    indicator.style.left = `${position.x * factors.x}px`;
-    indicator.style.top = `${position.y * factors.y}px`;
+    indicator.style.left = `${position.x}px`;
+    indicator.style.top = `${position.y}px`;
     indicator.style.color = color;
 
     indicator.addEventListener(
