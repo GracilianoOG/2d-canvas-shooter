08f60e9ff5dd828c084bba283c84c83907c7ebf2
Gabriel <gabriel.gracilianomb@gmail.com>
2026-08-07 09:59:58 -0300

    Create Renderer to manage render systems

---
+++ src/game/systems/Renderer.js
@@ -0,0 +1,37 @@
+import { config } from "../config";
+import { TRANSPARENT_BLACK } from "../constants/colors";
+
+export class Renderer {
+  #canvas;
+  #ctx;
+  #settings;
+
+  constructor(canvas, settings) {
+    this.#canvas = canvas;
+    this.#ctx = canvas.ctx;
+    this.#settings = settings;
+  }
+
+  renderCanvas() {
+    const { width, height } = config;
+    this.#canvas.render();
+
+    if (this.#settings.trails) {
+      this.#ctx.fillStyle = TRANSPARENT_BLACK;
+      this.#ctx.fillRect(0, 0, width, height);
+    } else {
+      this.#ctx.clearRect(0, 0, width, height);
+    }
+  }
+
+  renderEntities(entities) {
+    for (let i = 0; i < entities.length; i++) {
+      entities[i].draw(this.#ctx);
+    }
+  }
+
+  render(entities) {
+    this.renderCanvas();
+    this.renderEntities(entities);
+  }
+}
