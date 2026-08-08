fe9e8b95476497a72a875e6f87daea35145cdd24
Gabriel <gabriel.gracilianomb@gmail.com>
2026-08-08 12:57:46 -0300

    Remove shotgun and bullet hell from weapon list

--- src/data/weapons.js
+++ src/data/weapons.js
@@ -1,14 +1,12 @@
-import { Shotgun } from "@/game/arsenal/guns/Shotgun";
 import { Gun } from "@/game/arsenal/guns/Gun";
 import { weaponData } from "./weaponData";
-import { BulletHell } from "@/game/arsenal/guns/BulletHell";
 
 export const weapons = {
   bazooka: new Gun(weaponData.bazooka),
-  broom: new Shotgun(weaponData.broom),
-  bouncy: new Shotgun(weaponData.bouncy),
+  broom: new Gun(weaponData.broom),
+  bouncy: new Gun(weaponData.bouncy),
   cannon: new Gun(weaponData.cannon),
-  hell: new BulletHell(weaponData.hell),
+  hell: new Gun(weaponData.hell),
   launcher: new Gun(weaponData.launcher),
   minigun: new Gun(weaponData.minigun),
   mine: new Gun(weaponData.mine),
@@ -16,11 +14,11 @@ export const weapons = {
   pistol: new Gun(weaponData.pistol),
   rifle: new Gun(weaponData.rifle),
   ricochet: new Gun(weaponData.ricochet),
-  super: new Shotgun(weaponData.super),
-  shotgun: new Shotgun(weaponData.shotgun),
+  super: new Gun(weaponData.super),
+  shotgun: new Gun(weaponData.shotgun),
   smg: new Gun(weaponData.smg),
-  sonar: new Shotgun(weaponData.sonar),
-  that: new Shotgun(weaponData.that),
+  sonar: new Gun(weaponData.sonar),
+  that: new Gun(weaponData.that),
 };
 
 export const weaponIds = Object.keys(weapons).filter((id) => id !== "pistol");
