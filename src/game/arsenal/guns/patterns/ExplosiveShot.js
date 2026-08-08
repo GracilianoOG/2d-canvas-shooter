81ad2b546de38a645d9b690c8ab0c7df9ac6d7a2
Gabriel <gabriel.gracilianomb@gmail.com>
2026-08-08 17:35:55 -0300

    Pass ammoType to explode

--- src/game/arsenal/guns/patterns/ExplosiveShot.js
+++ src/game/arsenal/guns/patterns/ExplosiveShot.js
@@ -3,9 +3,6 @@ import { Explosive } from "@/game/entities/projectiles/Explosive";
 export class ExplosiveShot {
   create(gun, x, y, _angle) {
     const amount = gun.options.bullets;
-    Explosive.explode(x, y, {
-      amount,
-      createFragments: (x, y, angle) => gun.ammoType.create(x, y, angle),
-    });
+    Explosive.explode(x, y, amount, gun.ammoType);
   }
 }
