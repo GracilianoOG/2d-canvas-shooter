fb09509abd9e63111cc72ef43111ef81b7664d94
Gabriel <gabriel.gracilianomb@gmail.com>
2026-08-11 10:03:31 -0300

    Create data for all items

---
+++ src/data/itemData.js
@@ -0,0 +1,46 @@
+import * as Colors from "@/game/constants/colors";
+
+export const itemData = {
+  fury: {
+    radius: 10,
+    color: Colors.ORANGE_RED,
+    label: "fury",
+    despawnTime: 8000,
+  },
+  life: {
+    radius: 10,
+    color: Colors.CRIMSON,
+    label: "life",
+    despawnTime: 8000,
+  },
+  nuke: {
+    radius: 10,
+    color: Colors.YELLOW,
+    label: "nuke",
+    despawnTime: 8000,
+  },
+  sentry: {
+    radius: 10,
+    color: Colors.ROYAL_BLUE,
+    label: "sentry",
+    despawnTime: 8000,
+  },
+  shards: {
+    radius: 10,
+    color: Colors.WHITE,
+    label: "shards",
+    despawnTime: 8000,
+  },
+  shield: {
+    radius: 10,
+    color: Colors.ENERGETIC_BLUE,
+    label: "shield",
+    despawnTime: 8000,
+  },
+  weapon: {
+    radius: 10,
+    color: Colors.CHARTREUSE,
+    label: "gun",
+    despawnTime: 8000,
+  },
+};
