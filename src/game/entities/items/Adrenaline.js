f785684f14a71aa2f2b36043bf5ba1b0a2b7c5e9
Gabriel <gabriel.gracilianomb@gmail.com>
2026-08-11 23:38:14 -0300

    Remove check from Adrenaline

--- src/game/entities/items/Adrenaline.js
+++ src/game/entities/items/Adrenaline.js
@@ -1,13 +1,5 @@
-import { eventManager } from "@/engine/systems/EventManager";
 import { Item } from "./Item";
 
-class Adrenaline extends Item {
-  check() {
-    eventManager.emit("furyCollected", {
-      amount: 10,
-      collect: this.collect.bind(this),
-    });
-  }
-}
+class Adrenaline extends Item {}
 
 export { Adrenaline };
