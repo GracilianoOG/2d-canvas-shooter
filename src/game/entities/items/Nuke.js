d29f8c404bde7bad8d9b8467c51f6e19ffa748db
Gabriel <gabriel.gracilianomb@gmail.com>
2026-08-11 23:59:50 -0300

    Remove check from Nuke

--- src/game/entities/items/Nuke.js
+++ src/game/entities/items/Nuke.js
@@ -1,13 +1,3 @@
 import { Item } from "./Item";
-import { entityManager } from "@/game/systems/EntityManager";
-import { Layers } from "@/game/constants/layers";
 
-export class Nuke extends Item {
-  check() {
-    for (const enemy of entityManager.get(Layers.ENEMIES)) {
-      enemy.takeDamage(enemy.health);
-    }
-
-    this.collect();
-  }
-}
+export class Nuke extends Item {}
