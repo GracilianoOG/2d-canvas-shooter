a39a99c78ee0da7a3c481120b42778258f74dc19
Gabriel <gabriel.gracilianomb@gmail.com>
2026-08-11 23:31:55 -0300

    Remove check from Shards

--- src/game/entities/items/Shards.js
+++ src/game/entities/items/Shards.js
@@ -1,9 +1,3 @@
 import { Item } from "./Item";
-import { eventManager } from "@/engine/systems/EventManager";
 
-export class Shards extends Item {
-  check() {
-    eventManager.emit("shardsCollected");
-    this.collect();
-  }
-}
+export class Shards extends Item {}
