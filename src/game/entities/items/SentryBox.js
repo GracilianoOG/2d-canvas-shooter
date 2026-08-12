26dd713c2d76f9c65afb020b0739f62e2515fe9f
Gabriel <gabriel.gracilianomb@gmail.com>
2026-08-11 23:52:34 -0300

    Remove check from SentryBox

--- src/game/entities/items/SentryBox.js
+++ src/game/entities/items/SentryBox.js
@@ -1,12 +1,3 @@
-import { POWDER_BLUE } from "@/game/constants/colors";
 import { Item } from "./Item";
-import { entityManager } from "@/game/systems/EntityManager";
-import { Sentry } from "../Sentry";
 
-export class SentryBox extends Item {
-  check() {
-    const turret = new Sentry(12, POWDER_BLUE);
-    entityManager.add(this.x, this.y, turret);
-    this.collect();
-  }
-}
+export class SentryBox extends Item {}
