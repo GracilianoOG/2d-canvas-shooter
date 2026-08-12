616550825f0dbe9c38d21c5f413547571d7f61e6
Gabriel <gabriel.gracilianomb@gmail.com>
2026-08-11 23:15:20 -0300

    Remove check method from Life

--- src/game/entities/items/Life.js
+++ src/game/entities/items/Life.js
@@ -1,10 +1,5 @@
-import { eventManager } from "@/engine/systems/EventManager";
 import { Item } from "./Item";
 
-class Life extends Item {
-  check() {
-    eventManager.emit("lifeCollected", { collect: this.collect.bind(this) });
-  }
-}
+class Life extends Item {}
 
 export { Life };
