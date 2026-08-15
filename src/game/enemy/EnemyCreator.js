ae1610c84dffb65c7175bfc1ea7c207e9112ae79
Gabriel <gabriel.gracilianomb@gmail.com>
2026-08-14 22:48:38 -0300

    Pass target to generated enemy

--- src/game/enemy/EnemyCreator.js
+++ src/game/enemy/EnemyCreator.js
@@ -151,7 +151,7 @@ class EnemyCreator {
 
     const enemyConfig = this.#randomizeEnemy();
     const position = this.#randomizePosition(enemyConfig.radius);
-    const enemy = new EnemyClass(enemyConfig).target(this.#target);
+    const enemy = new EnemyClass(enemyConfig, this.#target);
     entityManager.add(...position, enemy, Layers.ENEMIES);
   }
 
