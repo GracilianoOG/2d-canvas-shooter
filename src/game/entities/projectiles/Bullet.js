2b56a5f9fbfe03264df069f08b394cb9977f4894
Gabriel <gabriel.gracilianomb@gmail.com>
2026-08-16 17:41:40 -0300

    Move bounce method to Bullet

--- src/game/entities/projectiles/Bullet.js
+++ src/game/entities/projectiles/Bullet.js
@@ -31,6 +31,22 @@ class Bullet extends Projectile {
     }
   }
 
+  bounce() {
+    const { x: ballX, y: ballY, radius } = this;
+    const { width: canvasW, height: canvasH } = config;
+
+    const X_AXIS = ballX < radius || ballX + radius > canvasW;
+    const Y_AXIS = ballY < radius || ballY + radius > canvasH;
+
+    if (X_AXIS) {
+      this.angle = Math.PI - this.angle;
+    } else if (Y_AXIS) {
+      this.angle = -this.angle;
+    }
+
+    return X_AXIS || Y_AXIS;
+  }
+
   isOutOfCanvas() {
     const { x: bx, y: by, radius: br } = this;
     const { width: cw, height: ch } = config;
--- src/game/entities/projectiles/Flechette.js
+++ src/game/entities/projectiles/Flechette.js
@@ -1,4 +1,3 @@
-import { config } from "@/game/config";
 import { GRAY } from "../../constants/colors";
 import { Bullet } from "./Bullet";
 
@@ -8,22 +7,6 @@ export class Flechette extends Bullet {
   constructor(angle, data) {
     super(angle, data);
     this.#bounces = data.bounces;
-  }
-
-  bounce() {
-    const { x: ballX, y: ballY, radius } = this;
-    const { width: canvasW, height: canvasH } = config;
-
-    const X_AXIS = ballX < radius || ballX + radius > canvasW;
-    const Y_AXIS = ballY < radius || ballY + radius > canvasH;
-
-    if (X_AXIS) {
-      this.angle = Math.PI - this.angle;
-    } else if (Y_AXIS) {
-      this.angle = -this.angle;
-    }
-
-    return X_AXIS || Y_AXIS;
   }
 
   update(delta) {
