cbb319002716f0386990100e7d472af00e9dbef0
Gabriel <gabriel.gracilianomb@gmail.com>
2026-09-01 12:03:41 -0300

    Notify orb value and nothing more

--- src/game/entities/Orb.js
+++ src/game/entities/Orb.js
@@ -33,11 +33,15 @@ export class Orb extends Projectile {
   }
 
   #notify() {
-    this.#events.emit("score", {
-      color: this.color,
-      score: this.#value,
-      position: { x: this.x, y: this.y },
-    });
+    this.#events.emit(
+      "indicate",
+      {
+        x: this.x,
+        y: this.y,
+      },
+      this.#value,
+      this.color,
+    );
   }
 
   grab() {
