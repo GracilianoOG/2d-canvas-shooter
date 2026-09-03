31ed43a74f1345eaee1830ed2701c8d17d218a72
Gabriel <gabriel.gracilianomb@gmail.com>
2026-09-02 22:31:35 -0300

    Chance value and color of orbs

--- src/data/orbData.js
+++ src/data/orbData.js
@@ -3,15 +3,15 @@ import * as Colors from "@/game/constants/colors";
 export const orbData = {
   score1: {
     type: "score",
-    color: Colors.VERY_LIGHT_BLUE,
+    color: Colors.GOLDEN,
     radius: 6,
     speed: 300,
     range: 100,
-    value: 25,
+    value: 100,
   },
   score2: {
     type: "score",
-    color: Colors.GOLDEN,
+    color: Colors.VERY_LIGHT_BLUE,
     radius: 6,
     speed: 300,
     range: 100,
@@ -23,7 +23,7 @@ export const orbData = {
     radius: 6,
     speed: 300,
     range: 100,
-    value: 10,
+    value: 25,
   },
   energy: {
     type: "energy",
