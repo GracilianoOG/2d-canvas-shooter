1b9f3039b613f8f736cd4a6204ec47dfc46b6d93
Gabriel <gabriel.gracilianomb@gmail.com>
2026-09-06 12:51:56 -0300

    Set active and hover styles of cursor using js

--- src/game/ui/Cursor.js
+++ src/game/ui/Cursor.js
@@ -2,11 +2,22 @@ export class Cursor {
   #cursor;
 
   constructor() {
+    const CLICKABLES = "a,button";
     const cursor = document.createElement("div");
     cursor.classList.add("cursor");
 
     document.body.prepend(cursor);
-    document.addEventListener("mousemove", (e) => this.move(e));
+    document.addEventListener("mousedown", (e) => {
+      cursor.classList.add("cursor__down");
+    });
+    document.addEventListener("mouseup", (e) => {
+      cursor.classList.remove("cursor__hover");
+      cursor.classList.remove("cursor__down");
+    });
+    document.addEventListener("mousemove", (e) => {
+      cursor.classList.toggle("cursor__hover", e.target.closest(CLICKABLES));
+      this.move(e);
+    });
     document.addEventListener("mouseover", (e) => this.move(e), { once: true });
 
     this.#cursor = cursor;
