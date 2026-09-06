cf47b0fd2440aa9d310c9b73c913d95f0410c776
Gabriel <gabriel.gracilianomb@gmail.com>
2026-09-06 10:09:57 -0300

    Create Cursor class

---
+++ src/game/ui/Cursor.js
@@ -0,0 +1,19 @@
+export class Cursor {
+  #cursor;
+
+  constructor() {
+    const cursor = document.createElement("div");
+    cursor.classList.add("cursor");
+    document.body.prepend(cursor);
+    document.addEventListener("mousemove", (e) => this.move(e));
+    document.addEventListener("mouseover", (e) => this.move(e), { once: true });
+    this.#cursor = cursor;
+  }
+
+  move(e) {
+    const mouseY = e.clientY;
+    const mouseX = e.clientX;
+    const space = 6;
+    this.#cursor.style.translate = `${mouseX - space}px ${mouseY - space}px`;
+  }
+}
